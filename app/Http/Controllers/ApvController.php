1e07e2da2647127eb65c9bcfd2eecece04462dfd
= <=>
2020-03-30 12:21:59 +0700

    update apv controller

--- app/Http/Controllers/ApvController.php
+++ app/Http/Controllers/ApvController.php
@@ -13,6 +13,7 @@ class ApvController extends Controller
      *
      * @return void
      */
+    public $successStatus = 200;
     public function __construct()
     {
         $this->middleware('auth');
@@ -22,8 +23,18 @@ class ApvController extends Controller
 
         // $kode_lokasi= $request->input('kode_lokasi');
         try {
-            $aju = DB::select("select * from yk_pb_m a 
+            $aju = DB::select("select a.due_date,a.no_pb as no_bukti,'INPROG' as status,convert(varchar,a.tanggal,103) as tgl,convert(varchar,a.due_date,103) as tgl2,a.modul,b.kode_pp+' - '+b.nama as pp,'-' as no_dokumen,a.keterangan,a.nilai,c.nik+' - '+c.nama as pembuat,a.no_app2,a.kode_lokasi,convert(varchar,a.tgl_input,120) as tglinput,b.kode_pp 
+            from yk_pb_m a 
+            inner join pp b on a.kode_pp=b.kode_pp and a.kode_lokasi=b.kode_lokasi 
+            inner join karyawan c on a.nik_user=c.nik and a.kode_lokasi=c.kode_lokasi 
             where a.progress='1' and a.kode_lokasi='34' and a.modul in ('PBBAU','PBPR','PBINV') 					 
+            union 			
+            select a.due_date,a.no_panjar as no_bukti,'INPROG' as status,convert(varchar,a.tanggal,103) as tgl,convert(varchar,a.due_date,103) as tgl2,a.modul,b.kode_pp+' - '+b.nama as pp,'-' as no_dokumen,a.keterangan,a.nilai,c.nik+' - '+c.nama as pembuat,a.no_app2,a.kode_lokasi,convert(varchar,a.tgl_input,120) as tglinput,b.kode_pp 
+            from panjar2_m a 
+            inner join pp b on a.kode_pp=b.kode_pp and a.kode_lokasi=b.kode_lokasi 
+            inner join karyawan c on a.nik_buat=c.nik and a.kode_lokasi=c.kode_lokasi 
+            where a.progress='1' and a.kode_lokasi='34' and a.modul in ('PJAJU','PJPR') 
+            					 
             ");
             $aju = json_decode(json_encode($aju),true);
             $siswa = DevSiswa::all();
