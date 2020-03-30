1fb830c882b9ce693b5bfa54187f92eebecf9cbd
= <=>
2020-03-30 12:12:20 +0700

    update route dan apv controller

--- app/Http/Controllers/ApvController.php
+++ app/Http/Controllers/ApvController.php
@@ -19,38 +19,45 @@ class ApvController extends Controller
         $this->middleware('auth');
     }
 
-    public function pengajuan(Request $request){
+    public function pengajuan(){
 
-        $kode_lokasi= $request->input('kode_lokasi');
-        $aju = DB::select("select a.due_date,a.no_pb as no_bukti,'INPROG' as status,convert(varchar,a.tanggal,103) as tgl,convert(varchar,a.due_date,103) as tgl2,a.modul,b.kode_pp+' - '+b.nama as pp,'-' as no_dokumen,a.keterangan,a.nilai,c.nik+' - '+c.nama as pembuat,a.no_app2,a.kode_lokasi,convert(varchar,a.tgl_input,120) as tglinput,b.kode_pp 
-        from yk_pb_m a 
-        inner join pp b on a.kode_pp=b.kode_pp and a.kode_lokasi=b.kode_lokasi 
-        inner join karyawan c on a.nik_user=c.nik and a.kode_lokasi=c.kode_lokasi 
-        where a.progress='1' and a.kode_lokasi='$kode_lokasi' and a.modul in ('PBBAU','PBPR','PBINV') 					 
-        union 			
-        select a.due_date,a.no_panjar as no_bukti,'INPROG' as status,convert(varchar,a.tanggal,103) as tgl,convert(varchar,a.due_date,103) as tgl2,a.modul,b.kode_pp+' - '+b.nama as pp,'-' as no_dokumen,a.keterangan,a.nilai,c.nik+' - '+c.nama as pembuat,a.no_app2,a.kode_lokasi,convert(varchar,a.tgl_input,120) as tglinput,b.kode_pp 
-        from panjar2_m a 
-        inner join pp b on a.kode_pp=b.kode_pp and a.kode_lokasi=b.kode_lokasi 
-        inner join karyawan c on a.nik_buat=c.nik and a.kode_lokasi=c.kode_lokasi 
-        where a.progress='1' and a.kode_lokasi='$kode_lokasi' and a.modul in ('PJAJU','PJPR') 
-        order by tgl
-        ");
-        $aju = json_decode(json_encode($aju),true);
-        // $siswa = DevSiswa::all();
-        
-        if(count($aju) > 0){ //mengecek apakah data kosong atau tidak
-            $success['status'] = true;
-            $success['data'] = $aju;
-            $success['env']= env('DB_CONNECTION');
-            $success['message'] = "Success!";
+        // $kode_lokasi= $request->input('kode_lokasi');
+        try {
+            $aju = DB::select("select a.due_date,a.no_pb as no_bukti,'INPROG' as status,convert(varchar,a.tanggal,103) as tgl,convert(varchar,a.due_date,103) as tgl2,a.modul,b.kode_pp+' - '+b.nama as pp,'-' as no_dokumen,a.keterangan,a.nilai,c.nik+' - '+c.nama as pembuat,a.no_app2,a.kode_lokasi,convert(varchar,a.tgl_input,120) as tglinput,b.kode_pp 
+            from yk_pb_m a 
+            inner join pp b on a.kode_pp=b.kode_pp and a.kode_lokasi=b.kode_lokasi 
+            inner join karyawan c on a.nik_user=c.nik and a.kode_lokasi=c.kode_lokasi 
+            where a.progress='1' and a.kode_lokasi='34' and a.modul in ('PBBAU','PBPR','PBINV') 					 
+            union all		
+            select a.due_date,a.no_panjar as no_bukti,'INPROG' as status,convert(varchar,a.tanggal,103) as tgl,convert(varchar,a.due_date,103) as tgl2,a.modul,b.kode_pp+' - '+b.nama as pp,'-' as no_dokumen,a.keterangan,a.nilai,c.nik+' - '+c.nama as pembuat,a.no_app2,a.kode_lokasi,convert(varchar,a.tgl_input,120) as tglinput,b.kode_pp 
+            from panjar2_m a 
+            inner join pp b on a.kode_pp=b.kode_pp and a.kode_lokasi=b.kode_lokasi 
+            inner join karyawan c on a.nik_buat=c.nik and a.kode_lokasi=c.kode_lokasi 
+            where a.progress='1' and a.kode_lokasi='34' and a.modul in ('PJAJU','PJPR') 
+            order by tgl
+            ");
+            $aju = json_decode(json_encode($aju),true);
+            // $siswa = DevSiswa::all();
             
-            return response()->json(['success'=>$success], $this->successStatus);     
-        }
-        else{
-            $success['message'] = "Data Kosong!";
-            $success['status'] = true;
-            
-            return response()->json(['success'=>$success], $this->successStatus);
+            if(count($aju) > 0){ //mengecek apakah data kosong atau tidak
+                $success['status'] = true;
+                $success['data'] = $aju;
+                $success['env']= env('DB_CONNECTION');
+                $success['message'] = "Success!";
+                
+                return response()->json(['success'=>$success], $this->successStatus);     
+            }
+            else{
+                $success['message'] = "Data Kosong!";
+                $success['status'] = true;
+                
+                return response()->json(['success'=>$success], $this->successStatus);
+            }
+        } catch (\Throwable $e) {
+            DB::rollback();
+            $success['status'] = false;
+            $success['message'] = "Error ".$e;
+            return response()->json($success, $this->successStatus);
         }
     }
 
