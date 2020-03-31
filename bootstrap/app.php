71cf54cbd8fff0a94283dca8d99ed22654478543
= <=>
2020-03-31 08:05:21 +0700

    update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -72,14 +72,15 @@ $app->configure('app');
 |
 */
 
-// $app->middleware([
-//     App\Http\Middleware\ExampleMiddleware::class
-// ]);
-
-$app->routeMiddleware([
-    'auth' => App\Http\Middleware\Authenticate::class,
-    // 'cors' => App\Http\Middleware\CorsMiddleware::class
+$app->middleware([
+    // App\Http\Middleware\ExampleMiddleware::class
+    'cors' => App\Http\Middleware\CorsMiddleware::class
 ]);
+
+// $app->routeMiddleware([
+//     'auth' => App\Http\Middleware\Authenticate::class,
+//     // 'cors' => App\Http\Middleware\CorsMiddleware::class
+// ]);
 
 /*
 |--------------------------------------------------------------------------
