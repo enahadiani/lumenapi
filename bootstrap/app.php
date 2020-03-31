f5f4eb7f6986bc3095e8d7c1149e8270b7f4f6af
= <=>
2020-03-31 08:01:16 +0700

    update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -78,7 +78,7 @@ $app->configure('app');
 
 $app->routeMiddleware([
     'auth' => App\Http\Middleware\Authenticate::class,
-    'cors' => App\Http\Middleware\CorsMiddleware::class
+    // 'cors' => App\Http\Middleware\CorsMiddleware::class
 ]);
 
 /*
