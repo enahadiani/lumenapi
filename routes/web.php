ff7f289e9932718097fe9e3dd32358f8242faa85
= <=>
2020-04-02 11:34:13 +0700

    routes add if menu mobile x

--- routes/web.php
+++ routes/web.php
@@ -61,13 +61,13 @@ $router->group(['middleware' => 'auth','prefix' => 'api/approval'], function () 
             $row = json_decode(json_encode($sql),true);
             switch($row[0]["menu_mobile"]){
                 case 'APPSM' :
-                    return Http\Controllers\ApprovalController::pengajuan();
+                    return Http\Controllers\Approval\ApprovalController::pengajuan();
                 break;
                 case 'APPFIN' :
-                    return Http\Controllers\ApprovalController::pengajuanfinal();
+                    return Http\Controllers\Approval\ApprovalController::pengajuanfinal();
                 break;
                 case 'APPDIR' :
-                    return Http\Controllers\ApprovalController::pengajuandir();
+                    return Http\Controllers\Approval\ApprovalController::pengajuandir();
                 break;
             }
             // return $row[0]["menu_mobile"];
