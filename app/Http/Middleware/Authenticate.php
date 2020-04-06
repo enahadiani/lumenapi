ae80c193385a8bfa4a008a0b50c1c112e35fab56
= <=>
2020-04-06 07:52:36 +0700

    update cors

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -35,10 +35,30 @@ class Authenticate
      */
     public function handle($request, Closure $next, $guard = null)
     {
+        $headers = [
+            'Access-Control-Allow-Origin'      => '*',
+            'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Max-Age'           => '86400',
+            'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
+        ];
+
+        if ($request->isMethod('OPTIONS') || $request->getMethod() == "OPTIONS")
+        {
+           return response()->json('{"method":"OPTIONS"}', 200, $headers);
+        }
+
         if ($this->auth->guard($guard)->guest()) {
+                
             return response()->json(['message'=>'Unauthorized'], 401);
         }
 
-        return $next($request);
+        $response = $next($request);
+        foreach($headers as $key => $value)
+        {
+            $response->header($key, $value);
+        }
+
+        return $response;
     }
 }
