597f0d7080eca47eb59fd3ec7191d2a244043eee
enahadiani <=>
2020-05-12 10:46:00 +0700

    add api sekolah jurusan

---
+++ app/Http/Controllers/Sekolah/AngkatanController.php
@@ -0,0 +1,279 @@
+<?php
+
+namespace App\Http\Controllers\Sekolah;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB; 
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class AngkatanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public $successStatus = 200;
+
+    public function index(Request $request)
+    {
+        try {
+            
+            if($data =  Auth::guard('tarbak')->user()){
+                $nik= $data->nik;
+                $kode_lokasi= $data->kode_lokasi;
+            }
+            if(isset($request->kode_pp)){
+                $filter = "and a.kode_pp='$request->kode_pp' ";
+            }
+
+            $res = DB::connection('sqlsrvtarbak')->select( "select a.kode_akt,a.kode_pp+'-'+b.nama as pp, a.nama, a.kode_tingkat,a.flag_aktif from sis_angkat a
+            inner join pp b on a.kode_pp=b.kode_pp and a.kode_lokasi=b.kode_lokasi
+            where a.kode_lokasi='".$kode_lokasi."' $filter ");
+            $res = json_decode(json_encode($res),true);
+            
+            if(count($res) > 0){ //mengecek apakah data kosong atau tidak
+                $success['status'] = true;
+                $success['data'] = $res;
+                $success['message'] = "Success!";     
+            }
+            else{
+                $success['message'] = "Data Kosong!";
+                $success['data'] = [];
+                $success['status'] = true;
+            }
+            return response()->json(['success'=>$success], $this->successStatus);
+        } catch (\Throwable $e) {
+            $success['status'] = false;
+            $success['message'] = "Error ".$e;
+            return response()->json($success, $this->successStatus);
+        }
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'kode_akt' => 'required',
+            'kode_tingkat' => 'required',
+            'nama' => 'required',
+            'kode_pp' => 'required',
+            'flag_aktif' => 'required'
+        ]);
+
+        DB::connection('sqlsrvtarbak')->beginTransaction();
+        
+        try {
+            if($data =  Auth::guard('tarbak')->user()){
+                $nik= $data->nik;
+                $kode_lokasi= $data->kode_lokasi;
+            }
+            
+            $ins = DB::connection('sqlsrvtarbak')->insert('insert into sis_angkat (kode_lokasi,kode_akt,nama,kode_tingkat,kode_pp,flag_aktif) values (?, ?, ?, ?, ?, ?)', [$kode_lokasi,$request->kode_akt,$request->nama,$request->kode_tingkat,$request->kode_pp,$request->flag_aktif]);
+            
+            DB::connection('sqlsrvtarbak')->commit();
+            $success['status'] = true;
+            $success['message'] = "Data Angkatan berhasil disimpan";
+            return response()->json(['success'=>$success], $this->successStatus);     
+        } catch (\Throwable $e) {
+            DB::connection('sqlsrvtarbak')->rollback();
+            $success['status'] = false;
+            $success['message'] = "Data Angkatan gagal disimpan ".$e;
+            return response()->json(['success'=>$success], $this->successStatus); 
+        }				
+        
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Fs  $Fs
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request)
+    {
+        $this->validate($request, [
+            'kode_pp' => 'required',
+            'kode_akt' => 'required'
+        ]);
+        try {
+            
+            
+            if($data =  Auth::guard('tarbak')->user()){
+                $nik= $data->nik;
+                $kode_lokasi= $data->kode_lokasi;
+            }
+
+            $kode_pp = $request->kode_pp;
+            $kode_akt = $request->kode_akt;
+
+            $res = DB::connection('sqlsrvtarbak')->select("select kode_akt,nama, kode_tingkat,kode_pp,flag_aktif from sis_angkat where kode_akt ='".$kode_akt."' and kode_lokasi='".$kode_lokasi."'  and kode_pp='".$kode_pp."' ");
+            $res = json_decode(json_encode($res),true);
+            
+            if(count($res) > 0){ //mengecek apakah data kosong atau tidak
+                $success['status'] = true;
+                $success['data'] = $res;
+                $success['message'] = "Success!";
+                return response()->json(['success'=>$success], $this->successStatus);     
+            }
+            else{
+                $success['message'] = "Data Tidak ditemukan!";
+                $success['data'] = [];
+                $success['status'] = false;
+                return response()->json(['success'=>$success], $this->successStatus); 
+            }
+        } catch (\Throwable $e) {
+            $success['status'] = false;
+            $success['message'] = "Error ".$e;
+            return response()->json($success, $this->successStatus);
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Fs  $Fs
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Fs $Fs)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Fs  $Fs
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $this->validate($request, [
+            'kode_akt' => 'required',
+            'kode_tingkat' => 'required',
+            'nama' => 'required',
+            'kode_pp' => 'required',
+            'flag_aktif' => 'required'
+        ]);
+
+        DB::connection('sqlsrvtarbak')->beginTransaction();
+        
+        try {
+            if($data =  Auth::guard('tarbak')->user()){
+                $nik= $data->nik;
+                $kode_lokasi= $data->kode_lokasi;
+            }
+            
+            $del = DB::connection('sqlsrvtarbak')->table('sis_angkat')
+            ->where('kode_lokasi', $kode_lokasi)
+            ->where('kode_akt', $request->kode_akt)
+            ->where('kode_pp', $request->kode_pp)
+            ->delete();
+
+            $ins = DB::connection('sqlsrvtarbak')->insert('insert into sis_angkat (kode_lokasi,kode_akt,nama,kode_tingkat,kode_pp,flag_aktif) values (?, ?, ?, ?, ?, ?)', [$kode_lokasi,$request->kode_akt,$request->nama,$request->kode_tingkat,$request->kode_pp,$request->flag_aktif]);
+            
+            DB::connection('sqlsrvtarbak')->commit();
+            $success['status'] = true;
+            $success['message'] = "Data Angkatan berhasil diubah";
+            return response()->json(['success'=>$success], $this->successStatus); 
+        } catch (\Throwable $e) {
+            DB::connection('sqlsrvtarbak')->rollback();
+            $success['status'] = false;
+            $success['message'] = "Data Angkatan gagal diubah ".$e;
+            return response()->json(['success'=>$success], $this->successStatus); 
+        }	
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Fs  $Fs
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+        $this->validate($request, [
+            'kode_pp' => 'required',
+            'kode_akt' => 'required'
+        ]);
+        DB::connection('sqlsrvtarbak')->beginTransaction();
+        
+        try {
+            if($data =  Auth::guard('tarbak')->user()){
+                $nik= $data->nik;
+                $kode_lokasi= $data->kode_lokasi;
+            }
+            
+            $del = DB::connection('sqlsrvtarbak')->table('sis_angkat')
+            ->where('kode_lokasi', $kode_lokasi)
+            ->where('kode_akt', $request->kode_akt)
+            ->where('kode_pp', $request->kode_pp)
+            ->delete();
+
+            DB::connection('sqlsrvtarbak')->commit();
+            $success['status'] = true;
+            $success['message'] = "Data Angkatan berhasil dihapus";
+            
+            return response()->json(['success'=>$success], $this->successStatus); 
+        } catch (\Throwable $e) {
+            DB::connection('sqlsrvtarbak')->rollback();
+            $success['status'] = false;
+            $success['message'] = "Data Angkatan gagal dihapus ".$e;
+            
+            return response()->json(['success'=>$success], $this->successStatus); 
+        }	
+    }
+
+    public function getTingkat()
+    {
+        try {
+            
+            
+            if($data =  Auth::guard('tarbak')->user()){
+                $nik= $data->nik;
+                $kode_lokasi= $data->kode_lokasi;
+            }
+
+            $res = DB::connection('sqlsrvtarbak')->select("select kode_tingkat,nama from sis_tingkat where kode_lokasi='".$kode_lokasi."' 	 
+            ");
+            $res = json_decode(json_encode($res),true);
+            
+            if(count($res) > 0){ //mengecek apakah data kosong atau tidak
+                $success['status'] = true;
+                $success['data'] = $res;
+                $success['message'] = "Success!";
+                return response()->json(['success'=>$success], $this->successStatus);     
+            }
+            else{
+                $success['message'] = "Data Tidak ditemukan!";
+                $success['data'] = [];
+                $success['status'] = false;
+                return response()->json(['success'=>$success], $this->successStatus); 
+            }
+        } catch (\Throwable $e) {
+            $success['status'] = false;
+            $success['message'] = "Error ".$e;
+            return response()->json($success, $this->successStatus);
+        }
+    }
+
+}
---
+++ app/Http/Controllers/Sekolah/TahunAjaranController.php
@@ -0,0 +1,285 @@
+<?php
+
+namespace App\Http\Controllers\Sekolah;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB; 
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class TahunAjaranController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public $successStatus = 200;
+
+    public function index(Request $request)
+    {
+        try {
+            
+            if($data =  Auth::guard('tarbak')->user()){
+                $nik= $data->nik;
+                $kode_lokasi= $data->kode_lokasi;
+            }
+            if(isset($request->kode_pp)){
+                $filter = "and a.kode_pp='$request->kode_pp' ";
+            }else{
+                $filter = "";
+            }
+            $res = DB::connection('sqlsrvtarbak')->select("select a.kode_ta,a.kode_pp+'-'+b.nama as pp,a.nama,convert (varchar, a.tgl_mulai,103) as tgl_mulai,convert (varchar, a.tgl_akhir,103) as tgl_akhir, case when a.flag_aktif='1' then 'AKTIF' else 'NONAKTIF' end as sts
+            from sis_ta a 
+            inner join pp b on a.kode_pp=b.kode_pp and a.kode_lokasi=b.kode_lokasi
+            where a.kode_lokasi='$kode_lokasi'	$filter	 
+            ");
+            $res = json_decode(json_encode($res),true);
+            
+            if(count($res) > 0){ //mengecek apakah data kosong atau tidak
+                $success['status'] = true;
+                $success['data'] = $res;
+                $success['message'] = "Success!";     
+            }
+            else{
+                $success['message'] = "Data Kosong!";
+                $success['data'] = [];
+                $success['status'] = true;
+            }
+            return response()->json(['success'=>$success], $this->successStatus);
+        } catch (\Throwable $e) {
+            $success['status'] = false;
+            $success['message'] = "Error ".$e;
+            return response()->json($success, $this->successStatus);
+        }
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'kode_ta' => 'required',
+            'nama' => 'required',
+            'kode_pp' => 'required',
+            'tgl_awal' => 'required',
+            'tgl_akhir' => 'required',
+            'flag_aktif' => 'required'
+        ]);
+
+        DB::connection('sqlsrvtarbak')->beginTransaction();
+        
+        try {
+            if($data =  Auth::guard('tarbak')->user()){
+                $nik= $data->nik;
+                $kode_lokasi= $data->kode_lokasi;
+            }
+            
+            $ins = DB::connection('sqlsrvtarbak')->insert('insert into sis_ta (kode_ta,nama,kode_lokasi,kode_pp,tgl_mulai,tgl_akhir,flag_aktif) values (?, ?, ?, ?, ?, ?, ?)', [$request->kode_ta,$request->nama,$kode_lokasi,$request->kode_pp,$request->tgl_awal,$request->tgl_akhir,$request->flag_aktif]);
+            
+            DB::connection('sqlsrvtarbak')->commit();
+            $success['status'] = true;
+            $success['message'] = "Data Tahun Ajaran berhasil disimpan";
+            return response()->json(['success'=>$success], $this->successStatus);     
+        } catch (\Throwable $e) {
+            DB::connection('sqlsrvtarbak')->rollback();
+            $success['status'] = false;
+            $success['message'] = "Data Tahun Ajaran gagal disimpan ".$e;
+            return response()->json(['success'=>$success], $this->successStatus); 
+        }				
+        
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Fs  $Fs
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request)
+    {
+        $this->validate($request, [
+            'kode_pp' => 'required',
+            'kode_ta' => 'required'
+        ]);
+        try {
+            
+            
+            if($data =  Auth::guard('tarbak')->user()){
+                $nik= $data->nik;
+                $kode_lokasi= $data->kode_lokasi;
+            }
+
+            $kode_pp = $request->kode_pp;
+            $kode_ta = $request->kode_ta;
+
+            $res = DB::connection('sqlsrvtarbak')->select("select kode_pp,kode_ta,nama, flag_aktif,tgl_mulai,tgl_akhir from sis_ta where kode_ta ='".$kode_ta."' and kode_lokasi='".$kode_lokasi."'  and kode_pp='".$kode_pp."'	 
+            ");
+            $res = json_decode(json_encode($res),true);
+            
+            if(count($res) > 0){ //mengecek apakah data kosong atau tidak
+                $success['status'] = true;
+                $success['data'] = $res;
+                $success['message'] = "Success!";
+                return response()->json(['success'=>$success], $this->successStatus);     
+            }
+            else{
+                $success['message'] = "Data Tidak ditemukan!";
+                $success['data'] = [];
+                $success['status'] = false;
+                return response()->json(['success'=>$success], $this->successStatus); 
+            }
+        } catch (\Throwable $e) {
+            $success['status'] = false;
+            $success['message'] = "Error ".$e;
+            return response()->json($success, $this->successStatus);
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Fs  $Fs
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Fs $Fs)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Fs  $Fs
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $this->validate($request, [
+            'kode_ta' => 'required',
+            'nama' => 'required',
+            'kode_pp' => 'required',
+            'tgl_awal' => 'required',
+            'tgl_akhir' => 'required',
+            'flag_aktif' => 'required'
+        ]);
+
+        DB::connection('sqlsrvtarbak')->beginTransaction();
+        
+        try {
+            if($data =  Auth::guard('tarbak')->user()){
+                $nik= $data->nik;
+                $kode_lokasi= $data->kode_lokasi;
+            }
+            
+            $del = DB::connection('sqlsrvtarbak')->table('sis_ta')
+            ->where('kode_lokasi', $kode_lokasi)
+            ->where('kode_ta', $request->kode_ta)
+            ->where('kode_pp', $request->kode_pp)
+            ->delete();
+
+            $ins = DB::connection('sqlsrvtarbak')->insert('insert into sis_ta (kode_ta,nama,kode_lokasi,kode_pp,tgl_mulai,tgl_akhir,flag_aktif) values (?, ?, ?, ?, ?, ?, ?)', [$request->kode_ta,$request->nama,$kode_lokasi,$request->kode_pp,$request->tgl_awal,$request->tgl_akhir,$request->flag_aktif]);
+            
+            DB::connection('sqlsrvtarbak')->commit();
+            $success['status'] = true;
+            $success['message'] = "Data Tahun Ajaran berhasil diubah";
+            return response()->json(['success'=>$success], $this->successStatus); 
+        } catch (\Throwable $e) {
+            DB::connection('sqlsrvtarbak')->rollback();
+            $success['status'] = false;
+            $success['message'] = "Data Tahun Ajaran gagal diubah ".$e;
+            return response()->json(['success'=>$success], $this->successStatus); 
+        }	
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Fs  $Fs
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+        $this->validate($request, [
+            'kode_pp' => 'required',
+            'kode_ta' => 'required'
+        ]);
+        DB::connection('sqlsrvtarbak')->beginTransaction();
+        
+        try {
+            if($data =  Auth::guard('tarbak')->user()){
+                $nik= $data->nik;
+                $kode_lokasi= $data->kode_lokasi;
+            }
+            
+            $del = DB::connection('sqlsrvtarbak')->table('sis_ta')
+            ->where('kode_lokasi', $kode_lokasi)
+            ->where('kode_ta', $request->kode_ta)
+            ->where('kode_pp', $request->kode_pp)
+            ->delete();
+
+            DB::connection('sqlsrvtarbak')->commit();
+            $success['status'] = true;
+            $success['message'] = "Data Tahun Ajaran berhasil dihapus";
+            
+            return response()->json(['success'=>$success], $this->successStatus); 
+        } catch (\Throwable $e) {
+            DB::connection('sqlsrvtarbak')->rollback();
+            $success['status'] = false;
+            $success['message'] = "Data Tahun Ajaran gagal dihapus ".$e;
+            
+            return response()->json(['success'=>$success], $this->successStatus); 
+        }	
+    }
+
+    public function getPP()
+    {
+        try {
+            
+            
+            if($data =  Auth::guard('tarbak')->user()){
+                $nik= $data->nik;
+                $kode_lokasi= $data->kode_lokasi;
+            }
+
+            $res = DB::connection('sqlsrvtarbak')->select("select kode_pp,nama from pp where kode_lokasi='".$kode_lokasi."' 	 
+            ");
+            $res = json_decode(json_encode($res),true);
+            
+            if(count($res) > 0){ //mengecek apakah data kosong atau tidak
+                $success['status'] = true;
+                $success['data'] = $res;
+                $success['message'] = "Success!";
+                return response()->json(['success'=>$success], $this->successStatus);     
+            }
+            else{
+                $success['message'] = "Data Tidak ditemukan!";
+                $success['data'] = [];
+                $success['status'] = false;
+                return response()->json(['success'=>$success], $this->successStatus); 
+            }
+        } catch (\Throwable $e) {
+            $success['status'] = false;
+            $success['message'] = "Error ".$e;
+            return response()->json($success, $this->successStatus);
+        }
+    }
+
+}
