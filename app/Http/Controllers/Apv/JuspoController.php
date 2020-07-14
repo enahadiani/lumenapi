822eae683ad2a11492fa05ddadeeab38346bf13c
enahadiani <=>
2020-07-14 15:42:13 +0700

    update api apv juspo

--- app/Http/Controllers/Apv/JuspoController.php
+++ app/Http/Controllers/Apv/JuspoController.php
@@ -326,15 +326,13 @@ class JuspoController extends Controller
             $res3 = DB::connection('sqlsrv2')->select($sql3);
             $res3 = json_decode(json_encode($res3),true);
             
-            $sql4="select a.no_bukti,case e.status when '2' then 'APPROVE' else 'REVISI' end as status,e.keterangan,c.nik,f.nama 
+            $sql4="select a.no_bukti,case e.status when '2' then 'APPROVE' when '3' then 'REVISI' else '-' end as status,e.keterangan,c.nik,f.nama 
             from apv_juspo_m a
-            left join (select no_bukti,kode_lokasi,max(id) as maxid
-                        from apv_pesan 
-                        group by no_bukti,kode_lokasi) d on a.no_bukti=d.no_bukti
-            left join apv_pesan e on d.no_bukti=e.no_bukti and d.maxid=e.id
-            left join apv_flow c on e.no_bukti=c.no_bukti and e.kode_lokasi=c.kode_lokasi and e.no_urut=c.no_urut
-            left join apv_karyawan f on c.nik=f.nik and c.kode_lokasi=f.kode_lokasi
-            where a.no_bukti='$no_bukti' and a.kode_lokasi='$kode_lokasi' ";
+            inner join apv_pesan e on a.no_bukti=e.no_bukti and a.kode_lokasi=e.kode_lokasi
+            inner join apv_flow c on e.no_bukti=c.no_bukti and e.kode_lokasi=c.kode_lokasi and e.no_urut=c.no_urut
+            inner join apv_karyawan f on c.nik=f.nik and c.kode_lokasi=f.kode_lokasi
+            where a.no_bukti='$no_bukti' and a.kode_lokasi='$kode_lokasi'
+			order by c.no_urut ";
             $res4 = DB::connection('sqlsrv2')->select($sql4);
             $res4 = json_decode(json_encode($res4),true);
             if(count($res) > 0){ //mengecek apakah data kosong atau tidak
