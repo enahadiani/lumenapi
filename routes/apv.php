57c0653aebde623efce79f53b9040210d4d27e1d
enahadiani <=>
2020-07-15 11:55:36 +0700

    update juskeb add kolom divisi

--- routes/apv.php
+++ routes/apv.php
@@ -79,6 +79,7 @@ $router->group(['middleware' => 'auth:admin'], function () use ($router) {
     $router->get('juskeb','Apv\JuskebController@index');
     $router->get('juskeb/{no_bukti}','Apv\JuskebController@show');
     $router->get('kota','Apv\JuskebController@getKota');
+    $router->get('divisi','Apv\JuskebController@getDivisi');
     $router->get('barang-klp','Apv\JuskebController@getBarangKlp');
     $router->get('generate-dok','Apv\JuskebController@generateDok');
     $router->post('juskeb','Apv\JuskebController@store');
