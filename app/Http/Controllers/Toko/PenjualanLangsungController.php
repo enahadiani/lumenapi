eb34e877362c11bfdd47d6e6a629e5d91658e0bc
enahadiani <=>
2020-07-22 16:07:41 +0700

    update api penjualan langsung

--- app/Http/Controllers/Toko/PenjualanLangsungController.php
+++ app/Http/Controllers/Toko/PenjualanLangsungController.php
@@ -77,7 +77,7 @@ class PenjualanLangsungController extends Controller
                 $ins = DB::connection($this->sql)->insert("insert into ol_cust(kode_cust,nama,alamat,no_tel,kode_lokasi,kota,provinsi) values ('$request->kode_cust','$request->nama_cust','$request->alamat_cust','$request->notel_cust','$kode_lokasi','$request->kota_cust','$request->prop_cust') ");
             }
 
-            $ins =DB::connection($this->sql)->insert("insert into ol_pesan_m (no_pesan,kode_lokasi,tanggal,kode_cust,nama_cust,notel_cust,alamat_cust,kota_cust,prop_cust,catatan,status_pesan,kode_kirim,no_resi,nilai_ongkir,nilai_pesan,no_ref1) values ('$id','$kode_lokasi',getdate(),'$request->kode_cust','$request->nama_cust','$request->notel_cust','$request->alamat_cust','$request->kota_cust','$request->prop_cust','$request->catatan','1','$request->kode_kirim','$request->no_resi',$request->nilai_ongkir,$request->nilai_pesan,'-') ");		
+            $ins =DB::connection($this->sql)->insert("insert into ol_pesan_m (no_pesan,kode_lokasi,tanggal,kode_cust,nama_cust,notel_cust,alamat_cust,kota_cust,prop_cust,catatan,status_pesan,kode_kirim,no_resi,nilai_ongkir,nilai_pesan,no_ref1) values ('$id','$kode_lokasi',getdate(),'$request->kode_cust','$request->nama_cust','$request->notel_cust','$request->alamat_cust','$request->kota_cust','$request->prop_cust','$request->catatan','input','$request->kode_kirim','$request->no_resi',$request->nilai_ongkir,$request->nilai_pesan,'-') ");		
 
             if(isset($request->kode_barang) && count($request->kode_barang) > 0){
 
