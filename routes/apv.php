f1025bd03c2080685f985a406a82891fe125ed5a
enahadiani <=>
2020-07-30 16:00:01 +0700

    update api karyawan & get notif

--- routes/apv.php
+++ routes/apv.php
@@ -170,6 +170,7 @@ $router->group(['middleware' => 'auth:silo'], function () use ($router) {
     //Laporan
     $router->get('lap-posisi','Apv\LaporanController@getPosisi');
     $router->post('notif', 'Apv\NotifController@sendNotif');
+    $router->get('notif', 'Apv\NotifController@getNotif');
 
 });
 
