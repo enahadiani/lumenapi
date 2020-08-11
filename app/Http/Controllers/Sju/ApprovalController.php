6a68dc2de7e7538203ab7e6cca994de1b02db2d3
enahadiani <=>
2020-08-11 15:32:14 +0700

    update vendor

--- app/Http/Controllers/Sju/ApprovalController.php
+++ app/Http/Controllers/Sju/ApprovalController.php
@@ -44,9 +44,9 @@ class ApprovalController extends Controller
             $aju = json_decode(json_encode($aju),true);
             
             if(count($aju) > 0){ //mengecek apakah data kosong atau tidak
-                for($i=0;$i<count($aju);$i++){
-                    $aju[$i]["nilai"] = number_format($aju[$i]["nilai"],0,",","."); 
-                }
+                // for($i=0;$i<count($aju);$i++){
+                //     $aju[$i]["nilai"] = number_format($aju[$i]["nilai"],0,",","."); 
+                // }
                 $success['status'] = true;
                 $success['data'] = $aju;
                 $success['message'] = "Success!";
@@ -91,9 +91,9 @@ class ApprovalController extends Controller
             $aju = json_decode(json_encode($aju),true);
             
             if(count($aju) > 0){ //mengecek apakah data kosong atau tidak
-                for($i=0;$i<count($aju);$i++){
-                    $aju[$i]["nilai"] = number_format($aju[$i]["nilai"],0,",","."); 
-                }
+                // for($i=0;$i<count($aju);$i++){
+                //     $aju[$i]["nilai"] = number_format($aju[$i]["nilai"],0,",","."); 
+                // }
                 $success['status'] = true;
                 $success['data'] = $aju;
                 $success['message'] = "Success!";
@@ -137,9 +137,9 @@ class ApprovalController extends Controller
             $aju = json_decode(json_encode($aju),true);
             
             if(count($aju) > 0){ //mengecek apakah data kosong atau tidak
-                for($i=0;$i<count($aju);$i++){
-                    $aju[$i]["nilai"] = number_format($aju[$i]["nilai"],0,",","."); 
-                }
+                // for($i=0;$i<count($aju);$i++){
+                //     $aju[$i]["nilai"] = number_format($aju[$i]["nilai"],0,",","."); 
+                // }
                 $success['status'] = true;
                 $success['data'] = $aju;
                 $success['message'] = "Success!";
@@ -183,9 +183,9 @@ class ApprovalController extends Controller
             $aju = json_decode(json_encode($aju),true);
             
             if(count($aju) > 0){ //mengecek apakah data kosong atau tidak
-                for($i=0;$i<count($aju);$i++){
-                    $aju[$i]["nilai"] = number_format($aju[$i]["nilai"],0,",","."); 
-                }
+                // for($i=0;$i<count($aju);$i++){
+                //     $aju[$i]["nilai"] = number_format($aju[$i]["nilai"],0,",","."); 
+                // }
                 $success['status'] = true;
                 $success['data'] = $aju;
                 $success['message'] = "Success!";
@@ -228,9 +228,9 @@ class ApprovalController extends Controller
             $aju = json_decode(json_encode($aju),true);
             
             if(count($aju) > 0){ //mengecek apakah data kosong atau tidak
-                for($i=0;$i<count($aju);$i++){
-                    $aju[$i]["nilai"] = number_format($aju[$i]["nilai"],0,",","."); 
-                }
+                // for($i=0;$i<count($aju);$i++){
+                //     $aju[$i]["nilai"] = number_format($aju[$i]["nilai"],0,",","."); 
+                // }
                 $success['status'] = true;
                 $success['data'] = $aju;
                 $success['message'] = "Success!";
@@ -324,11 +324,11 @@ class ApprovalController extends Controller
             $det = json_decode(json_encode($det),true);
             
             if(count($det) > 0){ //mengecek apakah data kosong atau tidak
-                for($i=0;$i<count($det);$i++){
-                    $det[$i]["nilai"] = number_format($det[$i]["nilai"],0,",","."); 
-                    $det[$i]["nilai_seb"] = number_format($det[$i]["nilai_seb"],0,",","."); 
-                    $det[$i]["potongan"] = number_format($det[$i]["potongan"],0,",","."); 
-                }
+                // for($i=0;$i<count($det);$i++){
+                //     $det[$i]["nilai"] = number_format($det[$i]["nilai"],0,",","."); 
+                //     $det[$i]["nilai_seb"] = number_format($det[$i]["nilai_seb"],0,",","."); 
+                //     $det[$i]["potongan"] = number_format($det[$i]["potongan"],0,",","."); 
+                // }
                 $success['status'] = true;
                 $success['data'] = $det;
                 $success['message'] = "Success!";
@@ -382,9 +382,9 @@ class ApprovalController extends Controller
             $det = json_decode(json_encode($det),true);
             
             if(count($det) > 0){ //mengecek apakah data kosong atau tidak
-                for($i=0;$i<count($det);$i++){
-                    $det[$i]["nilai"] = number_format($det[$i]["nilai"],0,",","."); 
-                }
+                // for($i=0;$i<count($det);$i++){
+                //     $det[$i]["nilai"] = number_format($det[$i]["nilai"],0,",","."); 
+                // }
                 $success['status'] = true;
                 $success['data'] = $det;
                 $success['message'] = "Success!";
@@ -509,11 +509,11 @@ class ApprovalController extends Controller
             $rek = json_decode(json_encode($rek),true);
             
             if(count($rek) > 0){ //mengecek apakah data kosong atau tidak
-                for($i=0;$i<count($rek);$i++){
-                    $rek[$i]["nilai"] = number_format($rek[$i]["nilai"],0,",","."); 
-                    $rek[$i]["nilai_seb"] = number_format($rek[$i]["nilai_seb"],0,",",".");
-                    $rek[$i]["potongan"] = number_format($rek[$i]["potongan"],0,",",".");  
-                }
+                // for($i=0;$i<count($rek);$i++){
+                //     $rek[$i]["nilai"] = number_format($rek[$i]["nilai"],0,",","."); 
+                //     $rek[$i]["nilai_seb"] = number_format($rek[$i]["nilai_seb"],0,",",".");
+                //     $rek[$i]["potongan"] = number_format($rek[$i]["potongan"],0,",",".");  
+                // }
                 $success['status'] = true;
                 $success['data'] = $rek;
                 $success['message'] = "Success!";
@@ -555,9 +555,9 @@ class ApprovalController extends Controller
             
             if(count($jur) > 0){ //mengecek apakah data kosong atau tidak
                 
-                for($i=0;$i<count($jur);$i++){
-                    $jur[$i]["nilai"] = number_format($jur[$i]["nilai"],0,",","."); 
-                }
+                // for($i=0;$i<count($jur);$i++){
+                //     $jur[$i]["nilai"] = number_format($jur[$i]["nilai"],0,",","."); 
+                // }
                 $success['status'] = true;
                 $success['data'] = $jur;
                 $success['message'] = "Success!";
