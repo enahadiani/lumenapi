3ad3dbdf1266117d185fb9ec5e61c0d68bcf224b
enahadiani <=>
2020-08-14 14:49:31 +0700

    update pembyaran api dago

--- app/Http/Controllers/Dago/PembayaranController.php
+++ app/Http/Controllers/Dago/PembayaranController.php
@@ -48,7 +48,7 @@ class PembayaranController extends Controller
             }
 
             $res = DB::connection($this->sql)->select("select a.no_kwitansi, a.tgl_bayar, a.no_reg, a.paket, a.jadwal, round(a.nilai_p,4) as nilai_p, a.nilai_t, (a.nilai_p * a.kurs) + a.nilai_t as total_idr 
-            from dgw_pembayaran a inner join trans_m b on a.no_kwitansi=b.no_bukti and a.kode_lokasi=b.kode_lokasi
+            from dgw_pembayaran a inner join trans_m b on a.no_kb=b.no_bukti and a.kode_lokasi=b.kode_lokasi
             where b.kode_lokasi='".$kode_lokasi."' and b.posted='F' and b.form='KBREG' ");
             $res = json_decode(json_encode($res),true);
             
