739d116fef5df5ecd2dbab4774c575e7edf2d19c
enahadiani <=>
2020-08-18 12:08:05 +0700

    update api juspo

--- app/Http/Controllers/Apv/JuspoController.php
+++ app/Http/Controllers/Apv/JuspoController.php
@@ -114,6 +114,7 @@ class JuspoController extends Controller
             $res = DB::connection($this->db)->select($sql);
 
             $res = json_decode(json_encode($res),true);
+
             
             if(count($res) > 0){ //mengecek apakah data kosong atau tidak
                 $success['status'] = true;
@@ -201,6 +202,8 @@ class JuspoController extends Controller
             }
 
             $inshis = DB::connection($this->db)->insert("insert into apv_juspo_his (no_juspo,keterangan,status,kode_lokasi,nik_user,tgl_input) values ('$no_bukti','$request->keterangan','$request->status','$kode_lokasi','$nik_user',getdate())");
+
+            $ins = DB::connection($this->db)->insert("insert into apv_pesan (no_bukti,kode_lokasi,keterangan,tanggal,no_urut,status,modul) values ('$no_bukti','$kode_lokasi','$request->keterangan',getdate(),-1,'$request->status','PO') ");
 
             //cek status
             if($request->status == 2){
@@ -370,6 +373,11 @@ class JuspoController extends Controller
             
             $res = DB::connection($this->db)->select($sql);
             $res = json_decode(json_encode($res),true);
+            if(count($res)> 0){
+                $no_aju = $res[0]['no_juskeb'];
+            }else{
+                $no_aju = "-";
+            }
 
             $sql2="select a.no_bukti,a.barang,a.barang_klp,a.harga,a.jumlah,a.nilai,a.ppn,a.grand_total,b.nama as nama_klp 
             from apv_juspo_d a 
@@ -381,19 +389,42 @@ class JuspoController extends Controller
             $res3 = DB::connection($this->db)->select($sql3);
             $res3 = json_decode(json_encode($res3),true);
             
-            $sql4="select e.id,a.no_bukti,case e.status when '2' then 'APPROVE' when '3' then 'REVISI' else '-' end as status,e.keterangan,c.nik,f.nama,c.no_urut 
+            // $sql4="
+            
+            // select e.id,a.no_bukti,case e.status when '2' then 'APPROVE' when '3' then 'REVISI' else '-' end as status,e.keterangan,c.nik,f.nama,c.no_urut 
+            // from apv_juspo_m a
+            // inner join apv_pesan e on a.no_bukti=e.no_bukti and a.kode_lokasi=e.kode_lokasi
+            // inner join apv_flow c on e.no_bukti=c.no_bukti and e.kode_lokasi=c.kode_lokasi and e.no_urut=c.no_urut
+            // inner join apv_karyawan f on c.nik=f.nik and c.kode_lokasi=f.kode_lokasi
+            // where a.no_bukti='$no_bukti' and a.kode_lokasi='$kode_lokasi'
+            // order by id,no_urut ";
+            $sql4 = "select e.no_bukti as id,a.no_bukti,case e.status when 'V' then 'APPROVE' when 'F' then 'REVISI' else '-' end as status,e.keterangan,e.nik_user as nik,f.nama,-3 as no_urut,-4 as id2 
+            from apv_juskeb_m a
+            inner join apv_ver_m e on a.no_bukti=e.no_juskeb and a.kode_lokasi=e.kode_lokasi
+            inner join apv_karyawan f on e.nik_user=f.nik and e.kode_lokasi=f.kode_lokasi
+            where a.no_bukti='$no_aju' and a.kode_lokasi='$kode_lokasi'
+			union all
+			select convert(varchar,e.id) as id,a.no_bukti,case e.status when '2' then 'APPROVE' when '3' then 'REVISI' else '-' end as status,e.keterangan,c.nik,f.nama,c.no_urut,e.id as id2 
+            from apv_juskeb_m a
+            inner join apv_pesan e on a.no_bukti=e.no_bukti and a.kode_lokasi=e.kode_lokasi
+            inner join apv_flow c on e.no_bukti=c.no_bukti and e.kode_lokasi=c.kode_lokasi and e.no_urut=c.no_urut
+            inner join apv_karyawan f on c.nik=f.nik and c.kode_lokasi=f.kode_lokasi
+            where a.no_bukti='$no_aju' and a.kode_lokasi='$kode_lokasi' 
+            union all
+select convert(varchar,e.id) as id,a.no_bukti,case e.status when '2' then 'APPROVE' when '3' then 'REVISI' else '-' end as status,e.keterangan,c.nik,f.nama,c.no_urut,e.id as id2 
             from apv_juspo_m a
             inner join apv_pesan e on a.no_bukti=e.no_bukti and a.kode_lokasi=e.kode_lokasi
             inner join apv_flow c on e.no_bukti=c.no_bukti and e.kode_lokasi=c.kode_lokasi and e.no_urut=c.no_urut
             inner join apv_karyawan f on c.nik=f.nik and c.kode_lokasi=f.kode_lokasi
             where a.no_bukti='$no_bukti' and a.kode_lokasi='$kode_lokasi'
 			union all
-			select e.id,a.no_bukti,case e.status when '2' then 'APPROVE' when '3' then 'REVISI' else '-' end as status,e.keterangan,e.nik_user,f.nama,-1 as no_urut 
+select convert(varchar,e.id) as id,a.no_bukti,case e.status when '2' then 'APPROVE' when '3' then 'REVISI' else '-' end as status,e.keterangan,a.nik_buat as nik,f.nama,-1 as no_urut,e.id as id2
             from apv_juspo_m a
-            inner join apv_juspo_his e on a.no_bukti=e.no_juspo and a.kode_lokasi=e.kode_lokasi
-            inner join apv_karyawan f on e.nik_user=f.nik and e.kode_lokasi=f.kode_lokasi
-            where a.no_bukti='$no_bukti' and a.kode_lokasi='$kode_lokasi'
-			order by id,no_urut ";
+            inner join apv_pesan e on a.no_bukti=e.no_bukti and a.kode_lokasi=e.kode_lokasi
+            inner join apv_karyawan f on a.nik_buat=f.nik and a.kode_lokasi=f.kode_lokasi
+            where a.no_bukti='$no_bukti' and a.kode_lokasi='$kode_lokasi' and e.modul='PO'
+			order by id2
+	        ";
             $res4 = DB::connection($this->db)->select($sql4);
             $res4 = json_decode(json_encode($res4),true);
             if(count($res) > 0){ //mengecek apakah data kosong atau tidak
@@ -445,12 +476,44 @@ class JuspoController extends Controller
             $sql3="select no_bukti,nama,file_dok from apv_juskeb_dok where kode_lokasi='".$kode_lokasi."' and no_bukti='$no_bukti'  order by no_urut";
             $res3 = DB::connection($this->db)->select($sql3);
             $res3 = json_decode(json_encode($res3),true);
+
+            $sql4 = "select e.no_bukti as id,a.no_bukti,case e.status when 'V' then 'APPROVE' when 'F' then 'REVISI' else '-' end as status,e.keterangan,e.nik_user as nik,f.nama,-3 as no_urut,-4 as id2 
+            from apv_juskeb_m a
+            inner join apv_ver_m e on a.no_bukti=e.no_juskeb and a.kode_lokasi=e.kode_lokasi
+            inner join apv_karyawan f on e.nik_user=f.nik and e.kode_lokasi=f.kode_lokasi
+            where a.no_bukti='$no_aju' and a.kode_lokasi='$kode_lokasi'
+			union all
+			select convert(varchar,e.id) as id,a.no_bukti,case e.status when '2' then 'APPROVE' when '3' then 'REVISI' else '-' end as status,e.keterangan,c.nik,f.nama,c.no_urut,e.id as id2 
+            from apv_juskeb_m a
+            inner join apv_pesan e on a.no_bukti=e.no_bukti and a.kode_lokasi=e.kode_lokasi
+            inner join apv_flow c on e.no_bukti=c.no_bukti and e.kode_lokasi=c.kode_lokasi and e.no_urut=c.no_urut
+            inner join apv_karyawan f on c.nik=f.nik and c.kode_lokasi=f.kode_lokasi
+            where a.no_bukti='$no_aju' and a.kode_lokasi='$kode_lokasi' 
+            union all
+select convert(varchar,e.id) as id,a.no_bukti,case e.status when '2' then 'APPROVE' when '3' then 'REVISI' else '-' end as status,e.keterangan,c.nik,f.nama,c.no_urut,e.id as id2 
+            from apv_juspo_m a
+            inner join apv_pesan e on a.no_bukti=e.no_bukti and a.kode_lokasi=e.kode_lokasi
+            inner join apv_flow c on e.no_bukti=c.no_bukti and e.kode_lokasi=c.kode_lokasi and e.no_urut=c.no_urut
+            inner join apv_karyawan f on c.nik=f.nik and c.kode_lokasi=f.kode_lokasi
+            where a.no_bukti='$no_bukti' and a.kode_lokasi='$kode_lokasi'
+			union all
+select convert(varchar,e.id) as id,a.no_bukti,case e.status when '2' then 'APPROVE' when '3' then 'REVISI' else '-' end as status,e.keterangan,a.nik_buat as nik,f.nama,-1 as no_urut,e.id as id2
+            from apv_juspo_m a
+            inner join apv_pesan e on a.no_bukti=e.no_bukti and a.kode_lokasi=e.kode_lokasi
+            inner join apv_karyawan f on a.nik_buat=f.nik and a.kode_lokasi=f.kode_lokasi
+            where a.no_bukti='$no_bukti' and a.kode_lokasi='$kode_lokasi' and e.modul='PO'
+			order by id2
+	        ";
+            $res4 = DB::connection($this->db)->select($sql4);
+            $res4 = json_decode(json_encode($res4),true);
+            
             
             if(count($res) > 0){ //mengecek apakah data kosong atau tidak
                 $success['status'] = true;
                 $success['data'] = $res;
                 $success['data_detail'] = $res2;
                 $success['data_dokumen'] = $res3;
+                $success['data_histori'] = $res4;
                 $success['message'] = "Success!";
                 return response()->json(['success'=>$success], $this->successStatus);     
             }
@@ -459,6 +522,7 @@ class JuspoController extends Controller
                 $success['data'] = [];
                 $success['data_detail'] = [];
                 $success['data_dokumen'] = [];
+                $success['data_histori'] = [];
                 $success['status'] = false;
                 return response()->json(['success'=>$success], $this->successStatus); 
             }
@@ -544,9 +608,10 @@ class JuspoController extends Controller
             $del = DB::connection($this->db)->table('apv_juspo_m')->where('kode_lokasi', $kode_lokasi)->where('no_bukti', $no_bukti)->delete();
             $del2 = DB::connection($this->db)->table('apv_juspo_d')->where('kode_lokasi', $kode_lokasi)->where('no_bukti', $no_bukti)->delete();
             $del3 = DB::connection($this->db)->table('apv_flow')->where('kode_lokasi', $kode_lokasi)->where('no_bukti', $no_bukti)->delete();
-            $del4 = DB::connection($this->db)->table('apv_juspo_his')->where('kode_lokasi', $kode_lokasi)->where('no_juspo', $no_bukti)->delete();
 
             $inshis = DB::connection($this->db)->insert("insert into apv_juspo_his (no_juspo,keterangan,status,kode_lokasi,nik_user,tgl_input) values ('$no_bukti','$request->keterangan','$request->status','$kode_lokasi','$nik_user',getdate())");
+
+            $ins = DB::connection($this->db)->insert("insert into apv_pesan (no_bukti,kode_lokasi,keterangan,tanggal,no_urut,status,modul) values ('$no_bukti','$kode_lokasi','$request->keterangan',getdate(),-1,'$request->status','PO') ");
 
             //cek status
             if($request->status == 2){
@@ -702,11 +767,7 @@ class JuspoController extends Controller
             inner join apv_pesan b on a.no_bukti=b.no_bukti and a.kode_lokasi=b.kode_lokasi and a.no_urut=b.no_urut
             left join apv_jab c on a.kode_jab=c.kode_jab and a.kode_lokasi=c.kode_lokasi
             where a.kode_lokasi='$kode_lokasi' and a.no_bukti='$no_bukti' 
-            union all 
-            select a.id,a.no_juspo as no_bukti,a.keterangan,a.tgl_input,c.nama
-            from apv_juspo_his a 
-            inner join apv_karyawan c on a.nik_user=c.nik and a.kode_lokasi=c.kode_lokasi
-            where a.kode_lokasi='$kode_lokasi' and a.no_juspo='$no_bukti' ";
+            ";
             
             $res = DB::connection($this->db)->select($sql);
             $res = json_decode(json_encode($res),true);
