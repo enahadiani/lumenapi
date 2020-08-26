f71783ad019df1c4c0a1c664954e799b47dbb24e
enahadiani <=>
2020-08-26 16:48:57 +0700

    update upload apv

--- app/Http/Controllers/Apv/JuskebController.php
+++ app/Http/Controllers/Apv/JuskebController.php
@@ -579,7 +579,7 @@ select convert(varchar,e.id) as id,a.no_bukti,case e.status when '2' then 'APPRO
                                 $arr_foto[] = $request->nama_file_seb[$i];
                             }     
                             $arr_nama[] = $request->input('nama_file')[$i];
-                            $arr_nama2[] = count($request->nama_file).'|'.$i;
+                            $arr_nama2[] = count($request->nama_file).'|'.$i.'|'.isset($request->file('file')[$i]);
                         }
     
                         $del3 = DB::connection($this->db)->table('apv_juskeb_dok')->where('kode_lokasi', $kode_lokasi)->where('no_bukti', $no_bukti)->delete();
