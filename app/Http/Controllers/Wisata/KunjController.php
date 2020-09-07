7dc65efa19f864691a299e4ee37ba8de84b0072e
mrthoriq <mrthoriq@mysai.co.id>
2020-09-07 15:55:42 +0700

    versi 1

--- app/Http/Controllers/Wisata/KunjController.php
+++ app/Http/Controllers/Wisata/KunjController.php
@@ -17,16 +17,7 @@ class KunjController extends Controller
     public $successStatus = 200;
     public $db = 'tokoaws';
     public $guard = 'toko';
-
-    // public function cekBukti($kode_mitra,$kode_bidang,$tahun,$bulan,$kode_lokasi){        
-    //     $auth = DB::connection($this->sql)->select("select no_bukti from par_kunj_m where kode_mitra ='".$kode_mitra."' and kode_bidang ='".$kode_bidang."' and tahun ='".$tahun."' and bulan ='".$bulan."' and kode_lokasi='".$kode_lokasi."' ");
-    //     $auth = json_decode(json_encode($auth),true);
-    //     if(count($auth) > 0){
-    //         return false;
-    //     }else{
-    //         return true;
-    //     }
-    // }
+    public $nobukti = '';
 
     public function getMitra() {
         try {
@@ -186,11 +177,11 @@ class KunjController extends Controller
                 $kode_lokasi= $data->kode_lokasi;
             }
 
-            if(isset($request->kode_mitra)){
-                if($request->kode_mitra == "all"){
+            if(isset($request->no_bukti)){
+                if($request->no_bukti == "all"){
                     $filter = "";
                 }else{
-                    $filter = " and a.kode_mitra='".$request->kode_mitra."' ";
+                    $filter = " and a.no_bukti='".$request->no_bukti."' ";
                 }
                 $sql= "select a.no_bukti,a.tahun,a.bulan,b.nama as nama_mitra, c.nama as nama_bidang 
                       from par_kunj_m a 
@@ -284,22 +275,38 @@ class KunjController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+
+    public function getBukti($kode_mitra,$kode_bidang,$tahun,$bulan,$kode_lokasi){            
+        $auth = DB::connection($this->db)->select("select no_bukti from par_kunj_m where kode_mitra ='".$kode_mitra."' and kode_bidang ='".$kode_bidang."' and tahun ='".$tahun."' and bulan ='".$bulan."' and kode_lokasi='".$kode_lokasi."' ");
+        $auth = json_decode(json_encode($auth),true);
+        if(count($auth) > 0){
+            return false;
+            $this->nobukti = $auth[0]["no_bukti"];
+        }else{
+            return true;
+        }
+    }
+
+    function generateKode($tabel, $kolom_acuan, $prefix, $str_format){
+        $query = DB::connection($this->db)->select("select right(max($kolom_acuan), ".strlen($str_format).")+1 as id from $tabel where $kolom_acuan like '$prefix%'");
+        $query = json_decode(json_encode($query),true);
+        $kode = $query[0]['id'];
+        $id = $prefix.str_pad($kode, strlen($str_format), $str_format, STR_PAD_LEFT);
+        return $id;
+    }
+
     public function store(Request $request)
     {
         $this->validate($request, [
+            'no_bukti' => 'required|max:20',
+            'tanggal' => 'required|date_format: Y-m-d',
             'kode_mitra' => 'required|max:10',
-            'nama' => 'required|max:100',
-            'alamat' => 'required|max:200',            
-            'no_tel' => 'required|max:50',            
-            'kecamatan' => 'required|max:200',            
-            'website' => 'required|max:200',            
-            'email' => 'required|max:100',            
-            'pic' => 'required|max:50', 
-            'no_hp' => 'required|max:50', 
-            'status' => 'required|max:50',             
-            'arrbidang'=>'required|array',
-            'arrbidang.*.kode_bidang' => 'required'            
-
+            'kode_bidang' => 'required|max:10',            
+            'tahun' => 'required|max:4',            
+            'bulan' => 'required|max:2',
+            'arrtgl'=>'required|array',
+            'arrtgl.*.tanggal' => 'required|date_format: Y-m-d',                                    
+            'arrtgl.*.jumlah' => 'required|number'                                    
         ]);
 
         DB::connection($this->db)->beginTransaction();
@@ -309,18 +316,23 @@ class KunjController extends Controller
                 $nik= $data->nik;
                 $kode_lokasi= $data->kode_lokasi;
             }
-            if($this->isUnik($request->kode_mitra,$kode_lokasi)){
+                       
+            if($this->getBukti($request->kode_mitra,$request->kode_bidang,$request->tahun,$request->bulan,$kode_lokasi)){
 
-                $ins = DB::connection($this->db)->insert("insert into par_mitra(kode_mitra,kode_lokasi,nama,alamat,no_tel,kecamatan,website,email,pic,no_hp,status,nik_user,tgl_input) values 
-                                                           ('".$request->kode_mitra."','".$kode_lokasi."','".$request->nama."','".$request->alamat."','".$request->no_tel."','".$request->kecamatan."','".$request->website."','".$request->email."','".$request->pic."','".$request->no_hp."','".$request->status."','".$nik."',getdate())");
+                $periode = substr($request->tanggal,2,2).substr($request->tanggal,5,2);
+                $no_bukti = $this->generateKode("pr_kunj_m", "no_bukti", $kode_lokasi."-KJ".$periode.".", "0001");
 
-                $arrbidang = $request->arrbidang;
-                if (count($arrbidang) > 0){
-                    for ($i=0;$i <count($arrbidang);$i++){                
-                        $ins2[$i] = DB::connection($this->db)->insert("insert into par_mitra_bid(kode_mitra,kode_bidang,kode_lokasi) values  
-                                                                        ('".$request->kode_mitra."','".$arrbidang[$i]['kode_bidang']."','".$kode_lokasi."')");                    
+                $ins = DB::connection($this->db)->insert("insert into par_kunj_m(no_bukti,tanggal,kode_lokasi,tgl_input,nik_user,kode_mitra,kode_bidang,tahun,bulan) values 
+                                                        ('".$no_bukti."','".$request->tanggal."','".$kode_lokasi."',getdate(),'".$nik."','".$request->kode_mitra."','".$request->kode_bidang."','".$request->tahun."','".$request->bulan."')");
+
+                $arrtgl = $request->arrtgl;
+                if (count($arrtgl) > 0){
+                    for ($i=0;$i <count($arrtgl);$i++){                
+                        $ins2[$i] = DB::connection($this->db)->insert("insert into par_kunj_m(no_bukti,kode_mitra,kode_bidang,tanggal,jumlah,kode_lokasi) values  
+                                                                      ('".$no_bukti."','".$request->kode_mitra."','".$request->kode_bidang."','".$arrtgl[$i]['tanggal']."','".floatval($arrtgl[$i]['jumlah'])."','".$kode_lokasi."')");                    
                     }						
-                }	                                          
+                }	
+                                                                      
                 DB::connection($this->db)->commit();
                 $success['status'] = true;
                 $success['message'] = "Data Mitra berhasil disimpan";
@@ -347,63 +359,63 @@ class KunjController extends Controller
      * @param  \App\Fs  $Fs
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request)
-    {
-        $this->validate($request, [
-            'kode_mitra' => 'required|max:10',
-            'nama' => 'required|max:100',
-            'alamat' => 'required|max:200',            
-            'no_tel' => 'required|max:50',            
-            'kecamatan' => 'required|max:200',            
-            'website' => 'required|max:200',            
-            'email' => 'required|max:100',            
-            'pic' => 'required|max:50', 
-            'no_hp' => 'required|max:50', 
-            'status' => 'required|max:50',
-            'arrbidang'=>'required|array',
-            'arrbidang.*.kode_bidang' => 'required'                        
-        ]);
+    // public function update(Request $request)
+    // {
+    //     $this->validate($request, [
+    //         'kode_mitra' => 'required|max:10',
+    //         'nama' => 'required|max:100',
+    //         'alamat' => 'required|max:200',            
+    //         'no_tel' => 'required|max:50',            
+    //         'kecamatan' => 'required|max:200',            
+    //         'website' => 'required|max:200',            
+    //         'email' => 'required|max:100',            
+    //         'pic' => 'required|max:50', 
+    //         'no_hp' => 'required|max:50', 
+    //         'status' => 'required|max:50',
+    //         'arrbidang'=>'required|array',
+    //         'arrbidang.*.kode_bidang' => 'required'                        
+    //     ]);
 
-        DB::connection($this->db)->beginTransaction();
+    //     DB::connection($this->db)->beginTransaction();
         
-        try {
-            if($data =  Auth::guard($this->guard)->user()){
-                $nik= $data->nik;
-                $kode_lokasi= $data->kode_lokasi;
-            }
+    //     try {
+    //         if($data =  Auth::guard($this->guard)->user()){
+    //             $nik= $data->nik;
+    //             $kode_lokasi= $data->kode_lokasi;
+    //         }
             
-            $del = DB::connection($this->db)->table('par_mitra')
-            ->where('kode_lokasi', $kode_lokasi)
-            ->where('kode_mitra', $request->kode_mitra)
-            ->delete();
+    //         $del = DB::connection($this->db)->table('par_mitra')
+    //         ->where('kode_lokasi', $kode_lokasi)
+    //         ->where('kode_mitra', $request->kode_mitra)
+    //         ->delete();
 
-            $del2 = DB::connection($this->db)->table('par_mitra_bid')
-            ->where('kode_lokasi', $kode_lokasi)
-            ->where('kode_mitra', $request->kode_mitra)
-            ->delete();
+    //         $del2 = DB::connection($this->db)->table('par_mitra_bid')
+    //         ->where('kode_lokasi', $kode_lokasi)
+    //         ->where('kode_mitra', $request->kode_mitra)
+    //         ->delete();
 
-            $ins = DB::connection($this->db)->insert("insert into par_mitra(kode_mitra,kode_lokasi,nama,alamat,no_tel,kecamatan,website,email,pic,no_hp,status,nik_user,tgl_input) values 
-                                                      ('".$request->kode_mitra."','".$kode_lokasi."','".$request->nama."','".$request->alamat."','".$request->no_tel."','".$request->kecamatan."','".$request->website."','".$request->email."','".$request->pic."','".$request->no_hp."','".$request->status."','".$nik."',getdate())");
+    //         $ins = DB::connection($this->db)->insert("insert into par_mitra(kode_mitra,kode_lokasi,nama,alamat,no_tel,kecamatan,website,email,pic,no_hp,status,nik_user,tgl_input) values 
+    //                                                   ('".$request->kode_mitra."','".$kode_lokasi."','".$request->nama."','".$request->alamat."','".$request->no_tel."','".$request->kecamatan."','".$request->website."','".$request->email."','".$request->pic."','".$request->no_hp."','".$request->status."','".$nik."',getdate())");
                                           
-            $arrbidang = $request->arrbidang;
-            if (count($arrbidang) > 0){
-                for ($i=0;$i <count($arrbidang);$i++){                
-                    $ins2[$i] = DB::connection($this->db)->insert("insert into par_mitra_bid(kode_mitra,kode_bidang,kode_lokasi) values  
-                                                                    ('".$request->kode_mitra."','".$arrbidang[$i]['kode_bidang']."','".$kode_lokasi."')");                    
-                }						
-            }
+    //         $arrbidang = $request->arrbidang;
+    //         if (count($arrbidang) > 0){
+    //             for ($i=0;$i <count($arrbidang);$i++){                
+    //                 $ins2[$i] = DB::connection($this->db)->insert("insert into par_mitra_bid(kode_mitra,kode_bidang,kode_lokasi) values  
+    //                                                                 ('".$request->kode_mitra."','".$arrbidang[$i]['kode_bidang']."','".$kode_lokasi."')");                    
+    //             }						
+    //         }
 
-            DB::connection($this->db)->commit();
-            $success['status'] = true;
-            $success['message'] = "Data Mitra berhasil diubah";
-            return response()->json($success, $this->successStatus); 
-        } catch (\Throwable $e) {
-            DB::connection($this->db)->rollback();
-            $success['status'] = false;
-            $success['message'] = "Data Mitra gagal diubah ".$e;
-            return response()->json($success, $this->successStatus); 
-        }	
-    }
+    //         DB::connection($this->db)->commit();
+    //         $success['status'] = true;
+    //         $success['message'] = "Data Mitra berhasil diubah";
+    //         return response()->json($success, $this->successStatus); 
+    //     } catch (\Throwable $e) {
+    //         DB::connection($this->db)->rollback();
+    //         $success['status'] = false;
+    //         $success['message'] = "Data Mitra gagal diubah ".$e;
+    //         return response()->json($success, $this->successStatus); 
+    //     }	
+    // }
 
     /**
      * Remove the specified resource from storage.
@@ -424,25 +436,25 @@ class KunjController extends Controller
                 $kode_lokasi= $data->kode_lokasi;
             }
             
-            $del = DB::connection($this->db)->table('par_mitra')
+            $del = DB::connection($this->db)->table('par_kunj_m')
             ->where('kode_lokasi', $kode_lokasi)
-            ->where('kode_mitra', $request->kode_mitra)
+            ->where('no_bukti', $this->nobukti)
             ->delete();
 
-            $del2 = DB::connection($this->db)->table('par_mitra_bid')
+            $del2 = DB::connection($this->db)->table('par_kunj_d')
             ->where('kode_lokasi', $kode_lokasi)
-            ->where('kode_mitra', $request->kode_mitra)
+            ->where('no_bukti', $this->nobukti)
             ->delete();
 
             DB::connection($this->db)->commit();
             $success['status'] = true;
-            $success['message'] = "Data Mitra berhasil dihapus";
+            $success['message'] = "Data Kunjungan berhasil dihapus";
             
             return response()->json($success, $this->successStatus); 
         } catch (\Throwable $e) {
             DB::connection($this->db)->rollback();
             $success['status'] = false;
-            $success['message'] = "Data Mitra gagal dihapus ".$e;
+            $success['message'] = "Data Kunjungan gagal dihapus ".$e;
             
             return response()->json($success, $this->successStatus); 
         }	
