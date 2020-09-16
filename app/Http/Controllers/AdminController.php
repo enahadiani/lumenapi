f487f4c4db326e9f9199a5645aa21ea0b8128463
enahadiani <=>
2020-09-16 16:35:29 +0700

    update esaku

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Admin;
 use Illuminate\Http\Request;
-
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB; 
-use  App\Admin;
+use Illuminate\Support\Facades\Storage; 
 
 class AdminController extends Controller
 {
