aeafc3282aa333e4c342bef898b74c988486957c
oktapascal <oktaiscool@gmail.com>
2020-09-23 15:21:07 +0700

    Testing query

--- app/Http/Controllers/Wisata/LaporanController.php
+++ app/Http/Controllers/Wisata/LaporanController.php
@@ -141,6 +141,7 @@ class LaporanController extends Controller
             if(count($res) > 0){ //mengecek apakah data kosong atau tidak
                 $success['status'] = true;
                 $success['data'] = $res;
+                $success['sql'] = $sql;
                 $success['message'] = "Success!";
                 $success["auth_status"] = 1;        
 
