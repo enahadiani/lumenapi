6ceb610fc40c319d11d8632079908304692c75e7
enahadiani <=>
2020-09-23 10:46:34 +0700

    update dago

--- app/Http/Controllers/Sekolah/PenilaianController.php
+++ app/Http/Controllers/Sekolah/PenilaianController.php
@@ -133,48 +133,50 @@ class PenilaianController extends Controller
                     $no_urut = 1;
                 }
 
-                $arr_foto = array();
-                $arr_nama = array();
-                $i=0;
-                if($request->hasfile('file'))
-                {
-                    foreach($request->file('file') as $file)
-                    {                
-                        $nama_foto = uniqid()."_".str_replace(' ', '_', $file->getClientOriginalName());
-                        $foto = $nama_foto;
-                        if(Storage::disk('s3')->exists('sekolah/'.$foto)){
-                            Storage::disk('s3')->delete('sekolah/'.$foto);
-                        }
-                        Storage::disk('s3')->put('sekolah/'.$foto,file_get_contents($file));
-                        $arr_foto[] = $foto;
-                        $arr_nama[] = str_replace(' ', '_', $request->input('nama_file')[$i]);
-                        $i++;
-                    }
-                }
+                // $arr_foto = array();
+                // $arr_nama = array();
+                // $i=0;
+                // if($request->hasfile('file'))
+                // {
+                //     foreach($request->file('file') as $file)
+                //     {                
+                //         $nama_foto = uniqid()."_".str_replace(' ', '_', $file->getClientOriginalName());
+                //         $foto = $nama_foto;
+                //         if(Storage::disk('s3')->exists('sekolah/'.$foto)){
+                //             Storage::disk('s3')->delete('sekolah/'.$foto);
+                //         }
+                //         Storage::disk('s3')->put('sekolah/'.$foto,file_get_contents($file));
+                //         $arr_foto[] = $foto;
+                //         $arr_nama[] = str_replace(' ', '_', $request->input('nama_file')[$i]);
+                //         $i++;
+                //     }
+                // }
 
-                $ins = DB::connection('sqlsrvtarbak')->insert("insert into sis_nilai_m(no_bukti,kode_ta,kode_kelas,kode_matpel,kode_jenis,kode_sem,tgl_input,nu,kode_lokasi,kode_pp) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ",array($no_bukti,$request->kode_ta,$request->kode_kelas,$request->kode_matpel,$request->kode_jenis,$request->kode_sem,date('Y-m-d H:i:s'),$no_urut,$kode_lokasi,$kode_pp));
+                $ins = DB::connection('sqlsrvtarbak')->insert("insert into sis_nilai_m(no_bukti,kode_ta,kode_kelas,kode_matpel,kode_jenis,kode_sem,tgl_input,nu,kode_lokasi,kode_pp) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ",array($no_bukti,$request->kode_ta,$request->kode_kelas,$request->kode_matpel,$request->kode_jenis,$request->kode_sem,date('Y-m-d H:i:s'),$no_urut,$kode_lokasi,$request->kode_pp));
                 for($i=0;$i<count($request->nis);$i++){
                     
                     $ins2[$i] = DB::connection('sqlsrvtarbak')->insert('insert into sis_nilai(no_bukti,nis,nilai,kode_lokasi,kode_pp) values (?, ?, ?, ?, ?)', array($no_bukti,$request->nis[$i],$request->nilai[$i],$kode_lokasi,$request->kode_pp));
                     
                 }  
 
-                if(count($arr_nama) > 0){
-                    for($i=0; $i<count($arr_nama);$i++){
-                        $ins3[$i] = DB::connection('sqlsrvtarbak')->insert("insert into sis_nilai_dok (
-                        no_bukti,kode_lokasi,file_dok,no_urut,nama,kode_pp,nis) values ('$no_bukti','$kode_lokasi','".$arr_foto[$i]."','".$i."','".$arr_nama[$i]."','$request->kode_pp','".$request->nis_dok[$i]."') "); 
-                    }
-                }
+                // if(count($arr_nama) > 0){
+                //     for($i=0; $i<count($arr_nama);$i++){
+                //         $ins3[$i] = DB::connection('sqlsrvtarbak')->insert("insert into sis_nilai_dok (
+                //         no_bukti,kode_lokasi,file_dok,no_urut,nama,kode_pp,nis) values ('$no_bukti','$kode_lokasi','".$arr_foto[$i]."','".$i."','".$arr_nama[$i]."','$request->kode_pp','".$request->nis_dok[$i]."') "); 
+                //     }
+                // }
 
                 DB::connection('sqlsrvtarbak')->commit();
                 $sts = true;
                 $msg = "Data Penilaian berhasil disimpan.";
             }else{
                 $sts = true;
+                $no_bukti = "-";
                 $msg = "Data Penilaian gagal disimpan. Detail Penilaian tidak valid";
             }
             
             $success['status'] = $sts;
+            $success['no_bukti'] = $no_bukti;
             $success['message'] = $msg;
             return response()->json(['success'=>$success], $this->successStatus);     
         } catch (\Throwable $e) {
@@ -206,21 +208,32 @@ class PenilaianController extends Controller
                 $kode_lokasi= $data->kode_lokasi;
             }
 
-            $res = DB::connection('sqlsrvtarbak')->select("select * from sis_nilai_m a where a.kode_lokasi='".$kode_lokasi."' and a.no_bukti='$request->no_bukti' and a.kode_pp='$request->kode_pp'  ");
+            $res = DB::connection('sqlsrvtarbak')->select("select a.no_bukti,a.kode_ta,a.kode_kelas,a.kode_jenis,a.kode_matpel,a.kode_sem,a.kode_pp,b.nama as nama_pp,c.nama as nama_ta,d.nama as nama_kelas,f.nama as nama_jenis,g.nama as nama_matpel,isnull(j.jumlah,0)+1 as jumlah
+            from sis_nilai_m a
+                inner join pp b on a.kode_pp=b.kode_pp and a.kode_lokasi=b.kode_lokasi
+                left join sis_ta c on a.kode_ta=c.kode_ta and a.kode_lokasi=c.kode_lokasi and a.kode_pp=c.kode_pp
+                left join sis_kelas d on a.kode_kelas=d.kode_kelas and a.kode_lokasi=d.kode_lokasi and a.kode_pp=d.kode_pp
+                left join sis_jenisnilai f on a.kode_jenis=f.kode_jenis and a.kode_lokasi=f.kode_lokasi and a.kode_pp=f.kode_pp
+                left join sis_matpel g on a.kode_matpel=g.kode_matpel and a.kode_lokasi=g.kode_lokasi and a.kode_pp=g.kode_pp
+                left join ( select kode_pp,kode_ta,kode_kelas,kode_sem,kode_matpel,kode_jenis,kode_lokasi,COUNT(*) as jumlah from sis_nilai_m 
+                    where no_bukti <> '$request->no_bukti'
+                    group by kode_pp,kode_ta,kode_kelas,kode_sem,kode_matpel,kode_jenis,kode_lokasi
+                    ) j on a.kode_ta=j.kode_ta and a.kode_lokasi=j.kode_lokasi and a.kode_pp=j.kode_pp and a.kode_jenis=j.kode_jenis and a.kode_sem=j.kode_sem and a.kode_matpel=j.kode_matpel and a.kode_kelas=j.kode_kelas
+            where a.kode_lokasi='".$kode_lokasi."' and a.no_bukti='$request->no_bukti' and a.kode_pp='$request->kode_pp'  ");
             $res = json_decode(json_encode($res),true);
 
             $res2 = DB::connection('sqlsrvtarbak')->select("select a.nis,a.nilai,b.nama from sis_nilai a inner join sis_siswa b on a.nis=b.nis where a.kode_lokasi = '".$kode_lokasi."' and a.no_bukti='$request->no_bukti' and a.kode_pp='$request->kode_pp'  ");
             $res2 = json_decode(json_encode($res2),true);
 
-            $res3 = DB::connection('sqlsrvtarbak')->select("select 
-            a.no_bukti,a.kode_lokasi,a.file_dok,a.no_urut,a.nama,a.kode_pp,a.nis from sis_nilai_dok a where a.kode_lokasi = '".$kode_lokasi."' and a.no_bukti='$request->no_bukti' and a.kode_pp='$request->kode_pp' ");
-            $res2 = json_decode(json_encode($res2),true);
+            // $res3 = DB::connection('sqlsrvtarbak')->select("select 
+            // a.no_bukti,a.kode_lokasi,a.file_dok,a.no_urut,a.nama,a.kode_pp,a.nis from sis_nilai_dok a where a.kode_lokasi = '".$kode_lokasi."' and a.no_bukti='$request->no_bukti' and a.kode_pp='$request->kode_pp' ");
+            // $res3 = json_decode(json_encode($res3),true);
             
             if(count($res) > 0){ //mengecek apakah data kosong atau tidak
                 $success['status'] = true;
                 $success['data'] = $res;
                 $success['data_detail'] = $res2;
-                $success['data_dok'] = $res3;
+                // $success['data_dok'] = $res3;
                 $success['message'] = "Success!";
                 return response()->json(['success'=>$success], $this->successStatus);     
             }
@@ -228,7 +241,7 @@ class PenilaianController extends Controller
                 $success['message'] = "Data Tidak ditemukan!";
                 $success['data'] = [];
                 $success['data_detail'] = [];
-                $success['data_dok'] = [];
+                // $success['data_dok'] = [];
                 $success['status'] = false;
                 return response()->json(['success'=>$success], $this->successStatus); 
             }
@@ -273,7 +286,7 @@ class PenilaianController extends Controller
             'nis_dok'=>'array|max:20',
             'file.*'=>'file|max:10240'
         ]);
-
+        
         DB::connection('sqlsrvtarbak')->beginTransaction();
         
         try {
@@ -284,47 +297,54 @@ class PenilaianController extends Controller
             
             
             if(count($request->nis) > 0){
-
+                
                 $no_bukti = $request->no_bukti;
-
-                $arr_foto = array();
-                $arr_nama = array();
-                $arr_foto2 = array();
-                $arr_nama2 = array();
-                $i=0;
-                $cek = $request->file;
-                //cek upload file tidak kosong
-                if(!empty($cek)){
-
-                    if(count($request->nama_file) > 0){
-                        //looping berdasarkan nama dok
-                        for($i=0;$i<count($request->nama_file);$i++){
-                            //cek row i ada file atau tidak
-                            if(isset($request->file('file')[$i])){
-                                $file = $request->file('file')[$i];
-
-                                //kalo ada cek nama sebelumnya ada atau -
-                                if($request->nama_file_seb[$i] != "-"){
-                                    //kalo ada hapus yang lama
-                                    Storage::disk('s3')->delete('sekolah/'.$request->nama_file_seb[$i]);
-                                }
-                                $nama_foto = uniqid()."_".str_replace(' ', '_', $file->getClientOriginalName());
-                                $foto = $nama_foto;
-                                if(Storage::disk('s3')->exists('sekolah/'.$foto)){
-                                    Storage::disk('s3')->delete('sekolah/'.$foto);
-                                }
-                                Storage::disk('s3')->put('sekolah/'.$foto,file_get_contents($file));
-                                $arr_foto[] = $foto;
-                            }else{
-                                $arr_foto[] = $request->nama_file_seb[$i];
-                            }     
-                            $arr_nama[] = $request->input('nama_file')[$i];
-                            $arr_nama2[] = count($request->nama_file).'|'.$i.'|'.isset($request->file('file')[$i]);
-                        }
-    
-                        $del3 = DB::connection('sqlsrvtarbak')->table('sis_nilai_dok')->where('kode_lokasi', $kode_lokasi)->where('no_bukti', $no_bukti)->where('kode_pp', $kode_pp)->delete();
-                    }
+                $strSQL = "select nu as jumlah from sis_nilai_m where no_bukti='$no_bukti' ";	
+                $cek = DB::connection('sqlsrvtarbak')->select($strSQL);
+                if(count($cek) > 0){
+                    $no_urut = $res[0]->jumlah;
+                }else{
+                    $no_urut = 1;
                 }
+
+                // $arr_foto = array();
+                // $arr_nama = array();
+                // $arr_foto2 = array();
+                // $arr_nama2 = array();
+                // $i=0;
+                // $cek = $request->file;
+                // //cek upload file tidak kosong
+                // if(!empty($cek)){
+
+                //     if(count($request->nama_file) > 0){
+                //         //looping berdasarkan nama dok
+                //         for($i=0;$i<count($request->nama_file);$i++){
+                //             //cek row i ada file atau tidak
+                //             if(isset($request->file('file')[$i])){
+                //                 $file = $request->file('file')[$i];
+
+                //                 //kalo ada cek nama sebelumnya ada atau -
+                //                 if($request->nama_file_seb[$i] != "-"){
+                //                     //kalo ada hapus yang lama
+                //                     Storage::disk('s3')->delete('sekolah/'.$request->nama_file_seb[$i]);
+                //                 }
+                //                 $nama_foto = uniqid()."_".str_replace(' ', '_', $file->getClientOriginalName());
+                //                 $foto = $nama_foto;
+                //                 if(Storage::disk('s3')->exists('sekolah/'.$foto)){
+                //                     Storage::disk('s3')->delete('sekolah/'.$foto);
+                //                 }
+                //                 Storage::disk('s3')->put('sekolah/'.$foto,file_get_contents($file));
+                //                 $arr_foto[] = $foto;
+                //             }else{
+                //                 $arr_foto[] = $request->nama_file_seb[$i];
+                //             }     
+                //             $arr_nama[] = $request->input('nama_file')[$i];
+                //             $arr_nama2[] = count($request->nama_file).'|'.$i.'|'.isset($request->file('file')[$i]);
+                //         }
+    
+                //         $del3 = DB::connection('sqlsrvtarbak')->table('sis_nilai_dok')->where('kode_lokasi', $kode_lokasi)->where('no_bukti', $no_bukti)->where('kode_pp', $kode_pp)->delete();
+                //     }
+                // }
 
                 $del = DB::connection('sqlsrvtarbak')->table('sis_nilai_m')
                 ->where('kode_lokasi', $kode_lokasi)
@@ -336,35 +356,31 @@ class PenilaianController extends Controller
                 ->where('no_bukti', $request->no_bukti)
                 ->delete();
 
-                $strSQL = "select nu as jumlah from sis_nilai_m where no_bukti='$no_bukti' ";	
-                $res = DB::connection('sqlsrvtarbak')->select($strSQL);
-                $res = json_decode(json_encode($res),true);
-            	if(count($res) > 0){
-                    $no_urut = $res[0]['jumlah'];
-                }
 
-                $ins = DB::connection('sqlsrvtarbak')->insert("insert into sis_nilai_m(no_bukti,kode_ta,kode_kelas,kode_matpel,kode_jenis,kode_sem,tgl_input,nu,kode_lokasi,kode_pp) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ",array($no_bukti,$request->kode_ta,$request->kode_kelas,$request->kode_matpel,$request->kode_jenis,$request->kode_sem,date('Y-m-d H:i:s'),$no_urut,$kode_lokasi,$kode_pp));
+                $ins = DB::connection('sqlsrvtarbak')->insert("insert into sis_nilai_m(no_bukti,kode_ta,kode_kelas,kode_matpel,kode_jenis,kode_sem,tgl_input,nu,kode_lokasi,kode_pp) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ",array($no_bukti,$request->kode_ta,$request->kode_kelas,$request->kode_matpel,$request->kode_jenis,$request->kode_sem,date('Y-m-d H:i:s'),$no_urut,$kode_lokasi,$request->kode_pp));
                 for($i=0;$i<count($request->nis);$i++){
-                    
-                    $ins[$i] = DB::connection('sqlsrvtarbak')->insert('insert into sis_nilai(no_bukti,nis,nilai,kode_lokasi,kode_pp) values (?, ?, ?, ?, ?)', array($no_bukti,$request->nis[$i],$request->nilai[$i],$kode_lokasi,$request->kode_pp));
+                    $ins2[$i] = DB::connection('sqlsrvtarbak')->insert('insert into sis_nilai(no_bukti,nis,nilai,kode_lokasi,kode_pp) values (?, ?, ?, ?, ?)', array($no_bukti,$request->nis[$i],$request->nilai[$i],$kode_lokasi,$request->kode_pp));
                     
                 }  
 
-                if(count($arr_nama) > 0){
-                    for($i=0; $i<count($arr_nama);$i++){
-                        $ins3[$i] = DB::connection('sqlsrvtarbak')->insert("insert into sis_nilai_dok (
-                        no_bukti,kode_lokasi,file_dok,no_urut,nama,kode_pp,nis) values ('$no_bukti','$kode_lokasi','".$arr_foto[$i]."','".$i."','".$arr_nama[$i]."','$request->kode_pp','".$request->nis_dok[$i]."') "); 
-                    }
-                }
+                // if(count($arr_nama) > 0){
+                //     for($i=0; $i<count($arr_nama);$i++){
+                //         $ins3[$i] = DB::connection('sqlsrvtarbak')->insert("insert into sis_nilai_dok (
+                //         no_bukti,kode_lokasi,file_dok,no_urut,nama,kode_pp,nis) values ('$no_bukti','$kode_lokasi','".$arr_foto[$i]."','".$i."','".$arr_nama[$i]."','$request->kode_pp','".$request->nis_dok[$i]."') "); 
+                //     }
+                // }
 
                 DB::connection('sqlsrvtarbak')->commit();
                 $sts = true;
                 $msg = "Data Penilaian berhasil diubah.";
+                
             }else{
                 $sts = true;
+                $no_bukti = "-";
                 $msg = "Data Penilaian gagal diubah. Detail Penilaian tidak valid";
             }
             
+            $success['no_bukti'] = $no_bukti;
             $success['status'] = $sts;
             $success['message'] = $msg;
      
@@ -409,21 +425,21 @@ class PenilaianController extends Controller
             ->where('kode_pp', $request->kode_pp)
             ->delete();
 
-            $sql3="select no_bukti,nama,file_dok from sis_nilai_dok where kode_lokasi='".$kode_lokasi."' and no_bukti='$no_bukti' and kode_pp='$kode_pp'  order by no_urut";
-            $res3 = DB::connection('sqlsrvtarbak')->select($sql3);
-            $res3 = json_decode(json_encode($res3),true);
+            // $sql3="select no_bukti,nama,file_dok from sis_nilai_dok where kode_lokasi='".$kode_lokasi."' and no_bukti='$no_bukti' and kode_pp='$kode_pp'  order by no_urut";
+            // $res3 = DB::connection('sqlsrvtarbak')->select($sql3);
+            // $res3 = json_decode(json_encode($res3),true);
 
-            if(count($res3) > 0){
-                for($i=0;$i<count($res3);$i++){
-                    Storage::disk('s3')->delete('sekolah/'.$res3[$i]['file_dok']);
-                }
-            }
+            // if(count($res3) > 0){
+            //     for($i=0;$i<count($res3);$i++){
+            //         Storage::disk('s3')->delete('sekolah/'.$res3[$i]['file_dok']);
+            //     }
+            // }
 
-            $del3 = DB::connection('sqlsrvtarbak')->table('sis_nilai_dok')
-            ->where('kode_lokasi', $kode_lokasi)
-            ->where('no_bukti', $no_bukti)
-            ->where('kode_pp', $request->kode_pp)
-            ->delete();
+            // $del3 = DB::connection('sqlsrvtarbak')->table('sis_nilai_dok')
+            // ->where('kode_lokasi', $kode_lokasi)
+            // ->where('no_bukti', $no_bukti)
+            // ->where('kode_pp', $request->kode_pp)
+            // ->delete();
 
             DB::connection('sqlsrvtarbak')->commit();
             $success['status'] = true;
