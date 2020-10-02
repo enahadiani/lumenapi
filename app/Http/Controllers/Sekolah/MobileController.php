aa127f929c4960582a3834629f1e3e1c17556a69
enahadiani <=>
2020-10-02 12:16:38 +0700

    add guru multi kelas

--- app/Http/Controllers/Sekolah/MobileController.php
+++ app/Http/Controllers/Sekolah/MobileController.php
@@ -1059,6 +1059,9 @@ class MobileController extends Controller
 
     public function getMatpel(Request $request)
     {
+        $this->validate($request,[
+            'kode_kelas' => 'required'
+        ]);
         try {
             
             if($data =  Auth::guard('siswa')->user()){
@@ -1074,9 +1077,10 @@ class MobileController extends Controller
                 $filter .= "";
             }
 
-            $res = DB::connection('sqlsrvtarbak')->select("select a.kode_matpel, a.nama
-            from sis_matpel a 
-            where a.kode_lokasi='".$kode_lokasi."' and a.kode_pp='$kode_pp' $filter ");
+            $res = DB::connection('sqlsrvtarbak')->select("select a.kode_matpel,b.nama 
+            from sis_guru_matpel_kelas a 
+            inner join sis_matpel b on a.kode_matpel=b.kode_matpel and a.kode_lokasi=b.kode_lokasi and a.kode_pp=b.kode_pp
+            where a.kode_lokasi='".$kode_lokasi."' and a.kode_pp='$kode_pp' and a.kode_kelas='$request->kode_kelas' $filter ");
             $res = json_decode(json_encode($res),true);
             
             if(count($res) > 0){ //mengecek apakah data kosong atau tidak
