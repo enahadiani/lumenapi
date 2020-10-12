6213b133896e89885777182deee1836bcf1ec7c7
mrthoriq <mrthoriq@mysai.co.id>
2020-10-12 14:26:36 +0700

    ver1

--- app/Http/Controllers/Barber/CustController.php
+++ app/Http/Controllers/Barber/CustController.php
@@ -169,7 +169,7 @@ class CustController extends Controller
             'kode_cust' => 'required|max:10',
             'nama' => 'required|max:100',
             'alamat' => 'required',
-            'no_hp' => 'reqiured',
+            'no_hp' => 'required',
             'flag_aktif' => 'required',
             'nik_user' => 'required'    
         ]);
