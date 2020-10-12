e1369f96b7bb108ee10c8bd3ecf163274c933330
enahadiani <=>
2020-10-12 16:57:09 +0700

    update lap dago

--- app/Http/Controllers/Sekolah/SisMatpelKhususController.php
+++ app/Http/Controllers/Sekolah/SisMatpelKhususController.php
@@ -32,7 +32,7 @@ class SisMatpelKhususController extends Controller
 
             $res = DB::connection('sqlsrvtarbak')->select("select distinct a.kode_kelas,a.nama,a.kode_pp+'-'+c.nama as pp,b.tgl_input,case when datediff(minute,b.tgl_input,getdate()) <= 10 then 'baru' else 'lama' end as status,case a.flag_aktif when 1 then 'AKTIF' else 'NONAKTIF' end as flag_aktif,b.kode_matpel+'-'+d.nama as kode_matpel,b.kode_ta,e.nama as nama_ta
             from sis_siswa_matpel_khusus b 
-            inner join sis_kelas a on a.kode_kelas=b.kode_kelas and a.kode_lokasi=b.kode_lokasi and a.kode_pp=b.kode_pp 
+            inner join sis_kelas_khusus a on a.kode_kelas=b.kode_kelas and a.kode_lokasi=b.kode_lokasi and a.kode_pp=b.kode_pp 
             inner join pp c on a.kode_lokasi=c.kode_lokasi and a.kode_pp=c.kode_pp  
             inner join sis_matpel d on b.kode_matpel=d.kode_matpel and b.kode_lokasi=d.kode_lokasi and b.kode_pp=d.kode_pp 
             inner join sis_ta e on b.kode_ta=e.kode_ta and b.kode_lokasi=e.kode_lokasi and b.kode_pp=e.kode_pp  
@@ -150,7 +150,7 @@ class SisMatpelKhususController extends Controller
             $res = DB::connection('sqlsrvtarbak')->select("select a.kode_pp, a.kode_matpel,b.nama as nama_pp,a.kode_ta,d.nama as nama_ta,a.kode_kelas,c.nama as nama_kelas,e.nama as nama_matpel 
             from sis_siswa_matpel_khusus a 
             inner join pp b on a.kode_pp=b.kode_pp and a.kode_lokasi=b.kode_lokasi
-            inner join sis_kelas c on a.kode_kelas=c.kode_kelas and a.kode_pp=c.kode_pp and a.kode_lokasi=c.kode_lokasi
+            inner join sis_kelas_khusus c on a.kode_kelas=c.kode_kelas and a.kode_pp=c.kode_pp and a.kode_lokasi=c.kode_lokasi
             inner join sis_ta d on a.kode_ta=d.kode_ta and a.kode_lokasi=d.kode_lokasi and a.kode_pp=d.kode_pp  
             inner join sis_matpel e on a.kode_matpel=e.kode_matpel and a.kode_lokasi=e.kode_lokasi and a.kode_pp=e.kode_pp  
             where a.kode_kelas='$kode_kelas' and a.kode_lokasi='".$kode_lokasi."' and a.kode_matpel='".$kode_matpel."' and a.kode_pp='".$kode_pp."' and a.kode_ta='".$kode_ta."' group by a.kode_pp,a.kode_kelas,b.nama,c.nama,a.kode_matpel,a.kode_ta,d.nama,e.nama");
