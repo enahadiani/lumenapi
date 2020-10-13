0f270fe16b610753e09e36776a2a177669a7434a
enahadiani <=>
2020-10-13 14:00:56 +0700

    update sekolah

--- app/Exports/NilaiExport.php
+++ app/Exports/NilaiExport.php
@@ -40,32 +40,13 @@ class NilaiExport implements FromCollection, WithHeadings, WithColumnFormatting,
     public function collection()
     {
         if($this->type == 'template'){
-            if($this->kode_kelas != null){
-                $res = DB::connection('sqlsrvtarbak')->table('sis_siswa')
-                        ->select('sis_siswa.nis','sis_siswa.nama')
-                        ->leftJoin('sis_nilai_tmp', function($join)
-                         {
-                             $join->on('sis_siswa.nis', '=', 'sis_nilai_tmp.nis');
-                             $join->on('sis_siswa.kode_lokasi','=','sis_nilai_tmp.kode_lokasi');
-                             $join->on('sis_siswa.kode_pp','=','sis_nilai_tmp.kode_pp');
-                         })
-                        ->where('sis_siswa.kode_lokasi',$this->kode_lokasi)
-                        ->where('sis_siswa.kode_kelas',$this->kode_kelas)
-                        ->where('sis_siswa.kode_pp',$this->kode_pp)
-                        ->get();
-            }else{
-                $res = DB::connection('sqlsrvtarbak')->table('sis_nilai_tmp')
-                        ->select('sis_nilai_tmp.nis','sis_siswa.nama')
-                        ->leftJoin('sis_siswa', function($join)
-                         {
-                             $join->on('sis_nilai_tmp.nis', '=', 'sis_siswa.nis');
-                             $join->on('sis_nilai_tmp.kode_lokasi','=','sis_siswa.kode_lokasi');
-                             $join->on('sis_nilai_tmp.kode_pp','=','sis_siswa.kode_pp');
-                         })
-                        ->where('sis_nilai_tmp.kode_lokasi','-')
-                        ->where('sis_nilai_tmp.nik_user',$this->nik_user)
-                        ->get();
-            }
+            $res = DB::connection('sqlsrvtarbak')->table('sis_siswa')
+                    ->select('sis_siswa.nis','sis_siswa.nama')
+                     ->where('sis_siswa.kode_kelas',$this->kode_kelas)
+                     ->where('sis_siswa.kode_lokasi',$this->kode_lokasi)
+                     ->where('sis_siswa.kode_pp',$this->kode_pp)
+                    ->get();
+            
         }else{
             $res = DB::connection('sqlsrvtarbak')->table('sis_nilai_tmp')
                         ->select('sis_nilai_tmp.nis','sis_siswa.nama','sis_nilai_tmp.nilai','sis_nilai_tmp.status','sis_nilai_tmp.keterangan','sis_nilai_tmp.nu')
