3b9aa758f0950dafc8ba2ef82428f6da5f000fab
oktapascal <oktaiscool@gmail.com>
2020-10-21 13:05:11 +0700

    Update

--- app/Http/Controllers/Dago/LaporanController.php
+++ app/Http/Controllers/Dago/LaporanController.php
@@ -420,6 +420,7 @@ class LaporanController extends Controller
             if(count($res) > 0){ //mengecek apakah data kosong atau tidak
                 $success['status'] = "SUCCESS";
                 $success['data'] = $res;
+                $success['sql'] = $sql;
                 $success['message'] = "Success!";
                 $success["auth_status"] = 1;    
                 return response()->json($success, $this->successStatus);     
