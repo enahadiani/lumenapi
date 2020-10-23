1512afb0073338e1e5273a645425b0bd9e5b6062
enahadiani <=>
2020-10-23 15:34:16 +0700

    update sekolah

--- app/Exports/NilaiExport.php
+++ app/Exports/NilaiExport.php
@@ -40,30 +40,35 @@ class NilaiExport implements FromCollection, WithHeadings, WithColumnFormatting,
     public function collection()
     {
         if($this->type == 'template'){
-            $res = DB::connection('sqlsrvtarbak')->table('sis_siswa')
-                    ->select('sis_siswa.nis','sis_siswa.nis2','sis_siswa.nama')
-                     ->where('sis_siswa.kode_kelas',$this->kode_kelas)
-                     ->where('sis_siswa.kode_lokasi',$this->kode_lokasi)
-                     ->where('sis_siswa.kode_pp',$this->kode_pp)
-                     ->where('sis_siswa.flag_aktif',1)
-                     ->orderBy('sis_siswa.nama')
-                    ->get();
+            // $res = DB::connection('sqlsrvtarbak')->table('sis_siswa')
+            //         ->select('sis_siswa.nis','sis_siswa.nis2','sis_siswa.nama')
+            //          ->where('sis_siswa.kode_kelas',$this->kode_kelas)
+            //          ->where('sis_siswa.kode_lokasi',$this->kode_lokasi)
+            //          ->where('sis_siswa.kode_pp',$this->kode_pp)
+            //          ->where('sis_siswa.flag_aktif',1)
+            //          ->orderBy('sis_siswa.nama')
+            //         ->get();
+            $res = collect(DB::connection('sqlsrvtarbak')->select("select a.nis as id,a.nis2 as nis,a.nama from sis_siswa a where a.kode_kelas ='$this->kode_kelas' and a.kode_lokasi ='$this->kode_lokasi' and a.kode_pp ='$this->kode_pp' and a.flag_aktif ='1' order by a.nama"));
             
         }else{
-            $res = DB::connection('sqlsrvtarbak')->table('sis_nilai_tmp')
-                        ->select('sis_nilai_tmp.nis','sis_siswa.nis2','sis_siswa.nama','sis_nilai_tmp.nilai','sis_nilai_tmp.status','sis_nilai_tmp.keterangan','sis_nilai_tmp.nu')
-                        ->leftJoin('sis_siswa', function($join)
-                         {
-                             $join->on('sis_nilai_tmp.nis', '=', 'sis_siswa.nis');
-                             $join->on('sis_nilai_tmp.kode_lokasi','=','sis_siswa.kode_lokasi');
-                             $join->on('sis_nilai_tmp.kode_pp','=','sis_siswa.kode_pp');
-                         })
-                        ->where('sis_nilai_tmp.kode_lokasi',$this->kode_lokasi)
-                        ->where('sis_nilai_tmp.nik_user',$this->nik_user)
-                        ->where('sis_nilai_tmp.kode_pp',$this->kode_pp)
-                        ->where('sis_siswa.flag_aktif',1)
-                        ->orderBy('sis_siswa.nama')
-                        ->get();
+            // $res = DB::connection('sqlsrvtarbak')->table('sis_nilai_tmp')
+            //             ->select('sis_nilai_tmp.nis','sis_siswa.nis2','sis_siswa.nama','sis_nilai_tmp.nilai','sis_nilai_tmp.status','sis_nilai_tmp.keterangan','sis_nilai_tmp.nu')
+            //             ->leftJoin('sis_siswa', function($join)
+            //              {
+            //                  $join->on('sis_nilai_tmp.nis', '=', 'sis_siswa.nis');
+            //                  $join->on('sis_nilai_tmp.kode_lokasi','=','sis_siswa.kode_lokasi');
+            //                  $join->on('sis_nilai_tmp.kode_pp','=','sis_siswa.kode_pp');
+            //              })
+            //             ->where('sis_nilai_tmp.kode_lokasi',$this->kode_lokasi)
+            //             ->where('sis_nilai_tmp.nik_user',$this->nik_user)
+            //             ->where('sis_nilai_tmp.kode_pp',$this->kode_pp)
+            //             ->where('sis_siswa.flag_aktif',1)
+            //             ->orderBy('sis_siswa.nama')
+            //             ->get();
+            $res = collect(DB::connection('sqlsrvtarbak')->select("select a.nis as id,b.nis2 as nis,b.nama,a.nilai,a.status,a.keterangan,a.nu 
+            from sis_nilai_tmp a
+            inner join sis_siswa b on a.nis=b.nis and a.kode_lokasi=b.kode_lokasi and a.kode_pp=b.kode_pp
+            where a.nik_user ='$this->nik_user' and a.kode_lokasi ='$this->kode_lokasi' and a.kode_pp ='$this->kode_pp' and b.flag_aktif ='1' order by b.nama"));
                         
         }
         return $res;
@@ -82,8 +87,8 @@ class NilaiExport implements FromCollection, WithHeadings, WithColumnFormatting,
                 [$this->kode_matpel, $this->kode_kelas,$this->kode_jenis,$this->kode_sem,$this->kode_kd],
                 ['','','','',''],
                 [
+                    'id',
                     'nis',
-                    'nis2',
                     'nama',
                     'nilai'
                 ]
@@ -94,8 +99,8 @@ class NilaiExport implements FromCollection, WithHeadings, WithColumnFormatting,
                 [$this->kode_matpel, $this->kode_kelas,$this->kode_jenis,$this->kode_sem,$this->kode_kd],
                 ['','','','',''],
                 [
+                    'id',
                     'nis',
-                    'nis2',
                     'nama',
                     'nilai',
                     'status',
