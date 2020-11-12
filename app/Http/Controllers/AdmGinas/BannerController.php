9f5943728721fde68681a5f63de4e28c61a43010
oktapascal <oktaiscool@gmail.com>
2020-11-12 13:45:12 +0700

    Update

--- app/Http/Controllers/AdmGinas/BannerController.php
+++ app/Http/Controllers/AdmGinas/BannerController.php
@@ -139,6 +139,39 @@ class BannerController extends Controller {
             return response()->json($success, $this->successStatus); 
         }	
     }
+
+    public function show(Request $request) {
+        $this->validate($request, [
+            'id_banner' => 'required'
+        ]);
+
+        try {
+            if($data =  Auth::guard($this->guard)->user()){
+                $nik= $data->nik;
+                $kode_lokasi= $data->kode_lokasi;
+            }
+
+            $res = DB::connection($this->db)->select("select file_gambar from lab_gbr_banner_detail where kode_lokasi = '$kode_lokasi' and id_banner = '$request->id_banner'");
+            
+            $res = json_decode(json_encode($res),true);
+            if(count($res) > 0){ //mengecek apakah data kosong atau tidak
+                $success['status'] = true;
+                $success['data'] = $res;
+                $success['message'] = "Success!";
+                return response()->json($success, $this->successStatus);     
+            }
+            else{
+                $success['message'] = "Data Kosong!";
+                $success['data'] = [];
+                $success['status'] = true;
+                return response()->json($success, $this->successStatus);
+            }
+        } catch (\Throwable $e) {
+            $success['status'] = false;
+            $success['message'] = "Error ".$e;
+            return response()->json($success, $this->successStatus);
+        }
+    }
 }
 
 ?>
