01229924c57030cacfa7d878b2a3805a4677b7d6
enahadiani <enahadiani2@gmail.com>
2020-11-24 09:38:03 +0700

    update api glitem

--- app/Http/Controllers/Yakes/GlitemController.php
+++ app/Http/Controllers/Yakes/GlitemController.php
@@ -36,14 +36,15 @@ class GlitemController extends Controller
             }
 
             if(count($request->data) > 0){
-                $del = DB::connection($this->db)->update("delete from exs_glitem where substring(pstng_date,1,6) = '$request->periode' ");
+                // $del = DB::connection($this->db)->update("delete from exs_glitem where substring(pstng_date,1,6) = '$request->periode' ");
     
-                for($j=1;$j < count($request->data); $j++){
-                    $dt = $request->data[$j];
-                    $ins[$j] = DB::connection($this->db)->insert("insert into exs_glitem (glacc,doc_no,fisc_year,assignment,pstng_date,doc_date,curr,doc_type,bus_area,amount,item_text,cost_ctr,profit_ctr,local_amount,kode_lokasi,tgl_update,tp,dc) 
-                    values ('".$dt["glacc"]."','".$dt["doc_no"]."','".$dt["fisc_year"]."','".$dt["assignment"]."','".$dt["pstng_date"]."','".$dt["doc_date"]."','".$dt["curr"]."','".$dt["doc_type"]."','".$dt["bus_area"]."','".floatval($dt["amount"])."','".$dt["item_text"]."','".$dt["cost_ctr"]."','".$dt["profit_ctr"]."','".floatval($dt["local_amount"])."','".$dt["kode_lokasi"]."','".$dt["tgl_update"]."','".$dt["tp"]."','".$dt["dc"]."')
-                    ");
-                }
+                // for($j=1;$j < count($request->data); $j++){
+                //     $dt = $request->data[$j];
+                //     $ins[$j] = DB::connection($this->db)->insert("insert into exs_glitem (glacc,doc_no,fisc_year,assignment,pstng_date,doc_date,curr,doc_type,bus_area,amount,item_text,cost_ctr,profit_ctr,local_amount,kode_lokasi,tgl_update,tp,dc) 
+                //     values ('".$dt["glacc"]."','".$dt["doc_no"]."','".$dt["fisc_year"]."','".$dt["assignment"]."','".$dt["pstng_date"]."','".$dt["doc_date"]."','".$dt["curr"]."','".$dt["doc_type"]."','".$dt["bus_area"]."','".floatval($dt["amount"])."','".$dt["item_text"]."','".$dt["cost_ctr"]."','".$dt["profit_ctr"]."','".floatval($dt["local_amount"])."','".$dt["kode_lokasi"]."','".$dt["tgl_update"]."','".$dt["tp"]."','".$dt["dc"]."')
+                //     ");
+                // }
+                $success['data'] = count($request->data);
     
                 DB::connection($this->db)->commit();
                 $success['status'] = true;
