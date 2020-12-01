9aa0fb25da17129865f1e2281736b56b6434462a
Okta Pascal Ibrahim <oktaiscool@gmail.com>
2020-12-01 10:37:15 +0700

    uPDATE

--- app/Http/Controllers/AdmGinas/BannerController.php
+++ app/Http/Controllers/AdmGinas/BannerController.php
@@ -59,69 +59,65 @@ class BannerController extends Controller {
             'mode' => 'required|array'
         ]);
 
-        dd($request->gambarke);
-        dd($request->id_banner);
-        dd($request->mode);
+        DB::connection($this->db)->beginTransaction();
 
-        // DB::connection($this->db)->beginTransaction();
+        try {
+            if($data =  Auth::guard($this->guard)->user()){
+                $nik= $data->nik;
+                $kode_lokasi= $data->kode_lokasi;
+            }
+            $arr_foto = array();
+            $arr_gambarke = array();
+            $i=0;
+            $cek = $request->file_gambar;
+            if(!empty($cek)){
+                if(count($request->gambarke) > 0){
+                    for($i=0;$i<count($request->gambarke);$i++){
+                        if(isset($request->file('file_gambar')[$i])){
 
-        // try {
-        //     if($data =  Auth::guard($this->guard)->user()){
-        //         $nik= $data->nik;
-        //         $kode_lokasi= $data->kode_lokasi;
-        //     }
-        //     $arr_foto = array();
-        //     $arr_gambarke = array();
-        //     $i=0;
-        //     $cek = $request->file_gambar;
-        //     if(!empty($cek)){
-        //         if(count($request->gambarke) > 0){
-        //             for($i=0;$i<count($request->gambarke);$i++){
-        //                 if(isset($request->file('file_gambar')[$i])){
-
-        //                     $file = $request->file('file_gambar')[$i];
-        //                     $foto = uniqid()."_".str_replace(' ', '_', $file->getClientOriginalName());
-        //                     if(Storage::disk('s3')->exists('webginas/'.$foto)){
-        //                         Storage::disk('s3')->delete('webginas/'.$foto);
-        //                     }
-        //                     Storage::disk('s3')->put('webginas/'.$foto,file_get_contents($file));
-        //                     $arr_foto[] = $foto;
-        //                 }else{
-        //                     $arr_foto[] = "-";
-        //                 }
-        //                 $arr_id_banner[] = $request->id_banner[$i];     
-        //                 $arr_mode[] = $request->mode[$i];
-        //             }
+                            $file = $request->file('file_gambar')[$i];
+                            $foto = uniqid()."_".str_replace(' ', '_', $file->getClientOriginalName());
+                            if(Storage::disk('s3')->exists('webginas/'.$foto)){
+                                Storage::disk('s3')->delete('webginas/'.$foto);
+                            }
+                            Storage::disk('s3')->put('webginas/'.$foto,file_get_contents($file));
+                            $arr_foto[] = $foto;
+                        }else{
+                            $arr_foto[] = "-";
+                        }
+                        $arr_id_banner[] = $request->id_banner[$i];     
+                        $arr_mode[] = $request->mode[$i];
+                    }
                     
-        //             $del = DB::connection($this->db)->table('lab_gbr_banner')->where('kode_lokasi', $kode_lokasi)->delete();
-        //         }
+                    $del = DB::connection($this->db)->table('lab_gbr_banner')->where('kode_lokasi', $kode_lokasi)->delete();
+                }
 
-        //         if(count($arr_gambarke) > 0){
-        //             for($i=0; $i<count($arr_gambarke);$i++){
-        //                 $ins[$i] = DB::connection($this->db)->insert("insert into lab_gbr_banner (id_banner,kode_lokasi,file_gambar,mode) values ('$arr_id_banner[$i]','$kode_lokasi',''$arr_foto[$i]'','$arr_mode[$i]') "); 
-        //             }
-        //             DB::connection($this->db)->commit();
-        //             $success['status'] = true;
-        //             $success['message'] = "Data Banner berhasil diupload.";
-        //             $success['no_bukti'] = "0";
-        //         }
-        //         else{
-        //             $success['status'] = false;
-        //             $success['message'] = "Data Banner gagal diupload. Banner file tidak valid. (2)";
-        //             $success['no_bukti'] = "0";
-        //         }
-        //     }else{
-        //         $success['status'] = false;
-        //         $success['message'] = "Data Banner gagal diupload. Banner file tidak valid. (3)";
-        //         $success['no_bukti'] = "0";
-        //     }
-        //     return response()->json($success, $this->successStatus);     
-        // } catch (\Throwable $e) {
-        //     DB::connection($this->db)->rollback();
-        //     $success['status'] = false;
-        //     $success['message'] = "Data Banner gagal disimpan ".$e;
-        //     return response()->json($success, $this->successStatus); 
-        // }	
+                if(count($arr_gambarke) > 0){
+                    for($i=0; $i<count($arr_gambarke);$i++){
+                        $ins[$i] = DB::connection($this->db)->insert("insert into lab_gbr_banner (id_banner,kode_lokasi,file_gambar,mode) values ('$arr_id_banner[$i]','$kode_lokasi',''$arr_foto[$i]'','$arr_mode[$i]') "); 
+                    }
+                    DB::connection($this->db)->commit();
+                    $success['status'] = true;
+                    $success['message'] = "Data Banner berhasil diupload.";
+                    $success['no_bukti'] = "0";
+                }
+                else{
+                    $success['status'] = false;
+                    $success['message'] = "Data Banner gagal diupload. Banner file tidak valid. (2)";
+                    $success['no_bukti'] = "0";
+                }
+            }else{
+                $success['status'] = false;
+                $success['message'] = "Data Banner gagal diupload. Banner file tidak valid. (3)";
+                $success['no_bukti'] = "0";
+            }
+            return response()->json($success, $this->successStatus);     
+        } catch (\Throwable $e) {
+            DB::connection($this->db)->rollback();
+            $success['status'] = false;
+            $success['message'] = "Data Banner gagal disimpan ".$e;
+            return response()->json($success, $this->successStatus); 
+        }	
     }
 }
 
