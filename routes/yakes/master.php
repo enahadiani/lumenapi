b4169e49e9f1f095cc0a731fa6f14f892a9a4935
mrthoriq <mrthoriq@mysai.co.id>
2020-12-04 15:49:45 +0700

    versi 1

--- routes/yakes/master.php
+++ routes/yakes/master.php
@@ -68,55 +68,6 @@ $router->group(['middleware' => 'auth:yakes'], function () use ($router) {
     $router->post('format-laporan-relasi','Yakes\FormatLaporanController@simpanRelasi');
     $router->post('format-laporan-move','Yakes\FormatLaporanController@simpanMove');
 
-    //hr dashborad
-    //stsorganik
-    $router->get('cariStsOrganik','Yakes\StsOrganikController@cariStsOrganik');
-    $router->get('stsOrganik','Yakes\StsOrganikController@index');
-    $router->post('stsOrganik','Yakes\StsOrganikController@store');
-    $router->put('stsOrganik','Yakes\StsOrganikController@update');
-    $router->delete('stsOrganik','Yakes\StsOrganikController@destroy'); 
-
-    //stsmedis
-    $router->get('cariStsMedis','Yakes\StsMedisController@cariStsMedis');
-    $router->get('stsMedis','Yakes\StsMedisController@index');
-    $router->post('stsMedis','Yakes\StsMedisController@store');
-    $router->put('stsMedis','Yakes\StsMedisController@update');
-    $router->delete('stsMedis','Yakes\StsOrganikController@destroy'); 
-
-    //stsedu
-    $router->get('cariStsEdu','Yakes\StsEduController@cariStsEdu');
-    $router->get('stsEdu','Yakes\StsEduController@index');
-    $router->post('stsEdu','Yakes\StsEduController@store');
-    $router->put('stsEdu','Yakes\StsEduController@update');
-    $router->delete('stsEdu','Yakes\StsEduController@destroy');
-    
-    //demografi
-    $router->get('cariDemog','Yakes\DemogController@cariDemog');
-    $router->get('demog','Yakes\DemogController@index');
-    $router->post('demog','Yakes\DemogController@store');
-    $router->put('demog','Yakes\DemogController@update');
-    $router->delete('demog','Yakes\DemogController@destroy');
-
-    //hrkaryawan
-    $router->get('cariNik','Yakes\HrKaryawanController@cariStsEdu');
-    $router->get('hrKaryawan','Yakes\HrKaryawanController@index');
-    $router->post('hrKaryawan','Yakes\HrKaryawanController@store');
-    $router->put('hrKaryawan','Yakes\HrKaryawanController@update');
-    $router->delete('hrKaryawan','Yakes\HrKaryawanController@destroy'); 
-
-    //dashbord akun
-    $router->get('dataBeban','Yakes\DashAkunController@dataBeban');
-    $router->get('dataPdpt','Yakes\DashAkunController@dataPdpt');
-
-    //dashbord SDM
-    $router->get('dataOrganik','Yakes\DashSDMController@dataOrganik');
-    $router->get('dataDemog','Yakes\DashSDMController@dataDemog');
-    $router->get('dataGender','Yakes\DashSDMController@dataGender');
-    $router->get('dataMedis','Yakes\DashSDMController@dataMedis');
-    $router->get('dataDokter','Yakes\DashSDMController@dataDokter');
-    $router->get('dataEdu','Yakes\DashSDMController@dataEdu');
-
-    
 });
 
 
