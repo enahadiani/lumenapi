09047e5dd944e0eeecc6df73e266f4498e456d87
enahadiani <enahadiani2@gmail.com>
2020-12-16 12:00:43 +0700

    update silo

--- app/Http/Controllers/Apv/JuspoController.php
+++ app/Http/Controllers/Apv/JuspoController.php
@@ -920,10 +920,10 @@ select convert(varchar,e.id) as id,a.no_bukti,case e.status when '2' then 'APPRO
 			left join apv_ver_m d on a.no_bukti=d.no_juskeb and a.kode_lokasi=d.kode_lokasi
             where a.kode_lokasi='$kode_lokasi' and a.no_bukti='$no_juskeb'
 			union all
-			select 'Diapprove oleh' as ket,a.kode_jab,c.nik,c.nama as nama_kar,b.nama as nama_jab,isnull(convert(varchar,a.tgl_app,103),'-') as tanggal,isnull(convert(varchar,d.maxid),'-') as no_app,case e.status when '2' then 'APPROVE' when '3' then 'REVISI' else '-' end as status,-2 as nu
+			select 'Diapprove oleh' as ket,a.kode_jab,a.nik,c.nama as nama_kar,b.nama as nama_jab,isnull(convert(varchar,a.tgl_app,103),'-') as tanggal,isnull(convert(varchar,d.maxid),'-') as no_app,case e.status when '2' then 'APPROVE' when '3' then 'REVISI' else '-' end as status,-2 as nu
             from apv_flow a
             inner join apv_jab b on a.kode_jab=b.kode_jab and a.kode_lokasi=b.kode_lokasi
-            inner join apv_karyawan c on a.kode_jab=c.kode_jab and a.kode_lokasi=c.kode_lokasi
+            inner join apv_karyawan c on a.nik=c.nik and a.kode_lokasi=c.kode_lokasi
 			left join (SELECT no_bukti,kode_lokasi,MAX(id) as maxid
                         FROM apv_pesan
                         GROUP BY no_bukti,kode_lokasi
