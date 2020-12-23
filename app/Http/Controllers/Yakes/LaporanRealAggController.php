021c862d6cba91b5fa163c9e5e013c86a216dfa2
enahadiani <enahadiani2@gmail.com>
2020-12-23 10:28:28 +0700

    update lap yakes

--- app/Http/Controllers/Yakes/LaporanRealAggController.php
+++ app/Http/Controllers/Yakes/LaporanRealAggController.php
@@ -20,6 +20,73 @@ class LaporanRealAggController extends Controller
     public $guard = 'yakes';
     public $sql = 'dbsapkug';
 
-    
+      
+    function getRealBeban(Request $request){
+        try {
+            
+            if($data =  Auth::guard($this->guard)->user()){
+                $nik= $data->nik;
+                $kode_lokasi= $data->kode_lokasi;
+            }
+
+            $nik_user=$request->nik_user;
+            $periode=$request->input('periode')[1];
+            $jenis=$request->input('jenis')[1];
+            if($jenis != "TOTAL"){
+                $filter_jenis = " and a.jenis='".$jenis."' ";
+            }else{
+                $filter_jenis = "";
+            }
+            $tahun = substr($periode,0,4);
+            $tahunseb = intval($tahun)-1;
+            $bulan = substr($periode,4,2);
+            $periodeseb = $tahunseb.$bulan;
+
+            $sql = "select a.kode_klpakun,a.nama
+            from dash_klp_akun a
+            where a.jenis='Beban'
+            order by a.kode_klpakun ";
+
+            $res = DB::connection($this->sql)->select($sql);
+            $res = json_decode(json_encode($res),true);
+
+            $sql2 = "select a.kode_akun,b.kode_klpakun,a.nama,isnull(b.rea_now,0) as rea_now,isnull(b.rea_bef,0) as rea_bef,isnull(b.rea_now,0)-isnull(b.rea_bef,0) as selisih
+            from masakun a
+            inner join (select a.kode_akun,b.kode_klpakun,a.kode_lokasi, 
+                           sum(case when periode between '".$tahun."01' and '$periode' then (case a.dc when 'D' then a.nilai else -a.nilai end) else 0 end) as rea_now,
+                           sum(case when periode between '".$tahunseb."01' and '$periodeseb' then (case a.dc when 'D' then a.nilai else -a.nilai end) else 0 end) as rea_bef
+                    from gldt a 
+                    inner join dash_klp_akun_d b on a.kode_akun=b.kode_akun
+                    inner join dash_klp_akun c on b.kode_klpakun=c.kode_klpakun
+                    where a.kode_lokasi='$kode_lokasi' and c.jenis in ('Beban','CC') 
+                    group by a.kode_akun,b.kode_klpakun,a.kode_lokasi
+                    )b on a.kode_lokasi=b.kode_lokasi and a.kode_akun=b.kode_akun
+            where a.kode_lokasi='$kode_lokasi'";
+
+            $res2 = DB::connection($this->sql)->select($sql2);
+            $res2 = json_decode(json_encode($res2),true);
+
+            if(count($res) > 0){ //mengecek apakah data kosong atau tidak
+                $success['data'] = $res;
+                $success['detail'] = $res2;
+                $success['status'] = true;
+                $success['message'] = "Success!";
+                $success["auth_status"] = 1;    
+                return response()->json($success, $this->successStatus);     
+            }
+            else{
+                $success['message'] = "Data Kosong!";
+                $success['data'] = [];
+                $success['detail'] = [];
+                $success['status'] = true;
+                $success["auth_status"] = 2;
+                return response()->json($success, $this->successStatus);
+            }
+        } catch (\Throwable $e) {
+            $success['status'] = false;
+            $success['message'] = "Error ".$e;
+            return response()->json($success, $this->successStatus);
+        }
+    }
 
 }
