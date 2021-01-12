84fc6cd066807f1713f0b703b63150c5284599fc
enahadiani <enahadiani2@gmail.com>
2021-01-12 16:56:12 +0700

    update periode aktif

--- app/Http/Controllers/Toko/PeriodeAktifController.php
+++ app/Http/Controllers/Toko/PeriodeAktifController.php
@@ -18,6 +18,17 @@ class PeriodeAktifController extends Controller
     public $sql = 'tokoaws';
     public $guard = 'toko';
 
+    public function isUnik($isi,$kode_lokasi){
+        
+        $auth = DB::connection($this->sql)->select("select modul from periode_aktif where modul ='".$isi."' and kode_lokasi='".$kode_lokasi."' ");
+        $auth = json_decode(json_encode($auth),true);
+        if(count($auth) > 0){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
     public function index(Request $request)
     {
         try {
@@ -27,9 +38,19 @@ class PeriodeAktifController extends Controller
                 $kode_lokasi= $data->kode_lokasi;
             }
 
-            
-            $sql = "select * from periode_aktif where kode_lokasi='".$kode_lokasi."' order by modul";
-            
+            if(isset($request->modul)){
+                if($request->modul == "all"){
+                    $filter = "";
+                }else{
+                    $filter = " and a.modul='$request->modul' ";
+                }
+                $sql= "select a.modul,a.keterangan,a.per_awal1,a.per_awal2,a.per_akhir1,a.per_akhir2
+                from periode_aktif a 
+                where a.kode_lokasi='".$kode_lokasi."' $filter ";
+            }else{
+                $sql = "select a.modul,a.keterangan,a.per_awal1,a.per_awal2,a.per_akhir1,a.per_akhir2,case when datediff(minute,a.tgl_input,getdate()) <= 10 then 'baru' else 'lama' end as status,a.tgl_input from periode_aktif a where a.kode_lokasi= '".$kode_lokasi."'";
+            }
+
             $res = DB::connection($this->sql)->select($sql);
             $res = json_decode(json_encode($res),true);
             
@@ -71,12 +92,12 @@ class PeriodeAktifController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
-            'modul' => 'required|array',
-            'keterangan' => 'required|array',
-            'per_awal1' => 'required|array',
-            'per_akhir1' => 'required|array',
-            'per_awal2' => 'required|array',
-            'per_akhir2' => 'required|array'
+            'modul' => 'required|max:10',
+            'keterangan' => 'required|max:50',
+            'per_awal1' => 'required|max:6',
+            'per_akhir1' => 'required|max:6',
+            'per_awal2' => 'required|max:6',
+            'per_akhir2' => 'required|max:6'
         ]);
 
         DB::connection($this->sql)->beginTransaction();
@@ -86,21 +107,21 @@ class PeriodeAktifController extends Controller
                 $nik= $data->nik;
                 $kode_lokasi= $data->kode_lokasi;
             }
+            if($this->isUnik($request->modul,$kode_lokasi)){
 
-            $del = DB::connection($this->sql)->table('periode_aktif')
-            ->where('kode_lokasi', $kode_lokasi)
-            ->delete();
-           
-            for ($i=0;$i < count($request->modul);$i++){
-
-                $ins[$i] = DB::connection($this->sql)->insert("insert into periode_aktif(kode_lokasi,modul,keterangan,per_awal1,per_akhir1,per_awal2,per_akhir2, nik_user,tgl_input) values('$kode_lokasi','".$request->modul[$i]."','".$request->keterangan[$i]."','".$request->per_awal1[$i]."','".$request->per_akhir1[$i]."','".$request->per_awal2[$i]."','".$request->per_akhir2[$i]."','$nik',getdate()) ");
+                $ins = DB::connection($this->sql)->insert("insert into periode_aktif(modul,keterangan,per_awal1,per_akhir1,per_awal2,per_akhir2,tgl_input,kode_lokasi,nik_user) values ('$request->modul','$request->keterangan','$request->per_awal1','$request->per_akhir1','$request->per_awal2','$request->per_akhir2',getdate(),'$kode_lokasi','$nik') ");
+                
+                DB::connection($this->sql)->commit();
+                $success['status'] = true;
+                $success['kode'] = $request->modul;
+                $success['message'] = "Data Periode Aktif berhasil disimpan";
+            }else{
+                $success['status'] = false;
+                $success['kode'] = "-";
+                $success['jenis'] = "duplicate";
+                $success['message'] = "Error : Duplicate entry. Modul sudah ada di database!";
             }
             
-            DB::connection($this->sql)->commit();
-            $success['status'] = true;
-            $success['kode'] = '-';
-            $success['message'] = "Data Periode Aktif berhasil disimpan";
-           
             return response()->json($success, $this->successStatus);     
         } catch (\Throwable $e) {
             DB::connection($this->sql)->rollback();
@@ -113,5 +134,133 @@ class PeriodeAktifController extends Controller
     }
 
 
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Fs  $Fs
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Fs $Fs)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Fs  $Fs
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $this->validate($request, [
+            'modul' => 'required|max:10',
+            'keterangan' => 'required|max:50',
+            'per_awal1' => 'required|max:6',
+            'per_akhir1' => 'required|max:6',
+            'per_awal2' => 'required|max:6',
+            'per_akhir2' => 'required|max:6'
+        ]);
+
+        DB::connection($this->sql)->beginTransaction();
+        
+        try {
+            if($data =  Auth::guard($this->guard)->user()){
+                $nik= $data->nik;
+                $kode_lokasi= $data->kode_lokasi;
+            }
+            
+            $del = DB::connection($this->sql)->table('periode_aktif')
+            ->where('kode_lokasi', $kode_lokasi)
+            ->where('modul', $request->modul)
+            ->delete();
+
+            $ins = DB::connection($this->sql)->insert("insert into periode_aktif(modul,keterangan,per_awal1,per_akhir1,per_awal2,per_akhir2,tgl_input,kode_lokasi,nik_user) values ('$request->modul','$request->keterangan','$request->per_awal1','$request->per_akhir1','$request->per_awal2','$request->per_akhir2',getdate(),'$kode_lokasi','$nik') ");
+            
+            DB::connection($this->sql)->commit();
+            $success['status'] = true;
+            $success['kode'] = $request->modul;
+            $success['message'] = "Data Periode Aktif berhasil diubah";
+            return response()->json($success, $this->successStatus); 
+        } catch (\Throwable $e) {
+            DB::connection($this->sql)->rollback();
+            $success['status'] = false;
+            $success['kode'] = "-";
+            $success['message'] = "Data Periode Aktif gagal diubah ".$e;
+            return response()->json($success, $this->successStatus); 
+        }	
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Fs  $Fs
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+        $this->validate($request, [
+            'modul' => 'required'
+        ]);
+        DB::connection($this->sql)->beginTransaction();
+        
+        try {
+            if($data =  Auth::guard($this->guard)->user()){
+                $nik= $data->nik;
+                $kode_lokasi= $data->kode_lokasi;
+            }
+            
+            $del = DB::connection($this->sql)->table('periode_aktif')
+            ->where('kode_lokasi', $kode_lokasi)
+            ->where('modul', $request->modul)
+            ->delete();
+
+            DB::connection($this->sql)->commit();
+            $success['status'] = true;
+            $success['message'] = "Data Periode Aktif berhasil dihapus";
+            
+            return response()->json($success, $this->successStatus); 
+        } catch (\Throwable $e) {
+            DB::connection($this->sql)->rollback();
+            $success['status'] = false;
+            $success['message'] = "Data Periode Aktif gagal dihapus ".$e;
+            
+            return response()->json($success, $this->successStatus); 
+        }	
+    }
+
+    public function getPeriode(Request $request)
+    {
+        try {
+            
+            if($data =  Auth::guard($this->guard)->user()){
+                $nik= $data->nik;
+                $kode_lokasi= $data->kode_lokasi;
+            }
+
+            $sql = "select distinct a.periode,dbo.fnNamaBulan(a.periode) as nama from trans_m a where a.periode <> '' and a.kode_lokasi='$kode_lokasi' ";
+
+            $res = DB::connection($this->sql)->select($sql);
+            $res = json_decode(json_encode($res),true);
+            
+            if(count($res) > 0){ //mengecek apakah data kosong atau tidak
+                $success['status'] = true;
+                $success['data'] = $res;
+                $success['message'] = "Success!";     
+            }
+            else{
+                $success['message'] = "Data Kosong!";
+                $success['data'] = [];
+                $success['status'] = false;
+            }
+            return response()->json($success, $this->successStatus);
+        } catch (\Throwable $e) {
+            $success['status'] = false;
+            $success['message'] = "Error ".$e;
+            return response()->json($success, $this->successStatus);
+        }
+        
+    }
 
 }
