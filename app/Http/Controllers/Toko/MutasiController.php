1015926f9bbd1cb22b548bc543d3031c7c74833d
Okta Pascal Ibrahim <oktaiscool@gmail.com>
2021-01-13 12:35:16 +0700

    Update

--- app/Http/Controllers/Toko/MutasiController.php
+++ app/Http/Controllers/Toko/MutasiController.php
@@ -35,7 +35,7 @@ class MutasiController extends Controller {
             $sql = "select distinct a.nama,a.sat_kecil,b.stok
                 from brg_barang a inner join brg_stok b on a.kode_barang=b.kode_barang and a.kode_lokasi=b.kode_lokasi 
                 and b.kode_gudang='$kode_gudang'
-                where a.kode_barang='$kode_barang' and a.kode_lokasi='$kode_lokasi' and b.nik_user = '$nik'";
+                where a.kode_barang='$kode_barang' and a.kode_lokasi='$kode_lokasi'";
 
             $res = DB::connection($this->sql)->select($sql);
             $res = json_decode(json_encode($res),true);
