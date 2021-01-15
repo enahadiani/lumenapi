f9f41dbb1d1030fff9dc45aa5fe0752dab811d16
enahadiani <enahadiani2@gmail.com>
2021-01-15 10:05:09 +0700

    add lap coa esaku

--- app/Http/Controllers/Toko/LaporanKeuLanjutController.php
+++ app/Http/Controllers/Toko/LaporanKeuLanjutController.php
@@ -200,6 +200,89 @@ class LaporanKeuLanjutController extends Controller
         }
     }
 
+    function getCOAStruktur(Request $request){
+        try {
+            
+            if($data =  Auth::guard($this->guard)->user()){
+                $nik= $data->nik;
+                $kode_lokasi= $data->kode_lokasi;
+            }
+
+            $col_array = array('kode_fs');
+            $db_col_name = array('a.kode_fs');
+            $where = "where a.kode_lokasi='$kode_lokasi'";
+            $this_in = "";
+            for($i = 0; $i<count($col_array); $i++){
+                if(ISSET($request->input($col_array[$i])[0])){
+                    if($request->input($col_array[$i])[0] == "range" AND ISSET($request->input($col_array[$i])[1]) AND ISSET($request->input($col_array[$i])[2])){
+                        $where .= " and (".$db_col_name[$i]." between '".$request->input($col_array[$i])[1]."' AND '".$request->input($col_array[$i])[2]."') ";
+                    }else if($request->input($col_array[$i])[0] == "=" AND ISSET($request->input($col_array[$i])[1])){
+                        $where .= " and ".$db_col_name[$i]." = '".$request->input($col_array[$i])[1]."' ";
+                    }else if($request->input($col_array[$i])[0] == "in" AND ISSET($request->input($col_array[$i])[1])){
+                        $tmp = explode(",",$request->input($col_array[$i])[1]);
+                        for($x=0;$x<count($tmp);$x++){
+                            if($x == 0){
+                                $this_in .= "'".$tmp[$x]."'";
+                            }else{
+            
+                                $this_in .= ","."'".$tmp[$x]."'";
+                            }
+                        }
+                        $where .= " and ".$db_col_name[$i]." in ($this_in) ";
+                    }
+                }
+            }
+            
+            $nik_user = $request->nik_user;
+
+            $sql = "select a.kode_neraca,a.nama,a.modul,a.tipe
+            from neraca a
+            $where and a.modul='A'
+            order by a.rowindex ";
+            $res = DB::connection($this->sql)->select($sql);
+            $res = json_decode(json_encode($res),true);
+
+            $sql2 ="select a.kode_neraca,a.nama,a.modul,a.tipe
+            from neraca a
+            $where and a.modul='P'
+            order by a.rowindex ";
+
+            $res2 = DB::connection($this->sql)->select($sql2);
+            $res2 = json_decode(json_encode($res2),true);
+
+            $sql3 ="select a.kode_neraca,a.nama,a.modul,a.tipe
+            from neraca a
+            $where and a.modul='L'
+            order by a.rowindex ";
+         
+            $res3 = DB::connection($this->sql)->select($sql3);
+            $res3 = json_decode(json_encode($res3),true);
+            
+            if(count($res) > 0){ //mengecek apakah data kosong atau tidak
+                $success['status'] = true;
+                $success['data'] = $res;
+                $success['data2'] = $res2;
+                $success['data3'] = $res3;
+                $success['message'] = "Success!";
+                $success["auth_status"] = 1;    
+                return response()->json($success, $this->successStatus);     
+            }
+            else{
+                $success['message'] = "Data Kosong!";
+                $success['data']=[];
+                $success['data2'] = [];
+                $success['data3'] = [];
+                $success['status'] = true;
+                $success["auth_status"] = 2;
+                return response()->json($success, $this->successStatus);
+            }
+        } catch (\Throwable $e) {
+            $success['status'] = false;
+            $success['message'] = "Error ".$e;
+            return response()->json($success, $this->successStatus);
+        }
+    }
+
     
     function getNrcLajurBulan(Request $request){
         try {
