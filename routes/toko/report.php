5ebdda164a624a59d514693c7e00398b5d65f414
enahadiani <enahadiani2@gmail.com>
2021-01-15 09:18:37 +0700

    update laporan esaku

--- routes/toko/report.php
+++ routes/toko/report.php
@@ -68,6 +68,7 @@ $router->group(['middleware' => 'auth:toko'], function () use ($router) {
     $router->get('lap-coa','Toko\LaporanKeuLanjutController@getCOA');
     $router->get('lap-nrclajur-bulan','Toko\LaporanKeuLanjutController@getNrcLajurBulan');
     $router->get('lap-labarugi-bulan','Toko\LaporanKeuLanjutController@getLabaRugiBulan');
+    $router->get('lap-neraca-bulan','Toko\LaporanKeuLanjutController@getNeracaBulan');
 
     
     $router->post('send-laporan','Toko\LaporanController@sendMail');
