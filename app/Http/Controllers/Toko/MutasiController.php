fc2bc51ac7e953fce694a741f07a401a1b19ca47
Okta Pascal Ibrahim <oktaiscool@gmail.com>
2021-01-15 16:43:49 +0700

    Update

--- app/Http/Controllers/Toko/MutasiController.php
+++ app/Http/Controllers/Toko/MutasiController.php
@@ -70,8 +70,12 @@ class MutasiController extends Controller {
 
             $sql1 = "select tanggal, substring(no_bukti,1,2) as jenis, no_bukti, no_dokumen, keterangan, param1, param2 
             from trans_m where no_bukti = '$no_bukti' and kode_lokasi = '$kode_lokasi'";
+            $sql3 = "select no_ref2 from trans_m no_bukti = '$no_bukti' and kode_lokasi = '$kode_lokasi'";
             $res = DB::connection($this->sql)->select($sql1);
             $res = json_decode(json_encode($res),true);
+
+            $res3 = DB::connection($this->sql)->select($sql3);
+            $res3 = json_decode(json_encode($res),true);
             
             if($res[0]['jenis'] == "MK") {
                 $sql2 = "select a.kode_barang,b.nama,a.satuan,a.jumlah,c.stok+a.jumlah as stok
@@ -82,7 +86,7 @@ class MutasiController extends Controller {
                 $sql2 = "select b.kode_barang,b.nama,a.satuan,c.jumlah as stok, a.jumlah
                     from brg_trans_d a
                     inner join brg_barang b on a.kode_barang=b.kode_barang and a.kode_lokasi=b.kode_lokasi
-                    inner join brg_trans_d c on c.no_bukti='$no_bukti'
+                    inner join brg_trans_d c on c.no_bukti='".$res3[0]['no_ref2']."'
                     and a.kode_lokasi=c.kode_lokasi and a.kode_barang=c.kode_barang
                     where a.no_bukti = '$no_bukti' and a.kode_lokasi='$kode_lokasi' order by a.nu";
             }
@@ -300,6 +304,7 @@ class MutasiController extends Controller {
             DB::connection($this->sql)->commit();
             $success['status'] = true;
             $success['no_bukti'] = $no_bukti;
+            $success['no_kirim'] = $data[0]['bukti_kirim'];
             $success['message'] = "Data Mutasi Barang berhasil disimpan ";
             return response()->json(['success'=>$success], $this->successStatus);
             
@@ -357,7 +362,7 @@ class MutasiController extends Controller {
                     nik1,nik2,nik3,no_ref1,no_ref2,no_ref3,param1,param2,param3,due_date,file_dok,id_sync) 
                     values ('".$data[0]['no_bukti']."', '$kode_lokasi', getdate(), '$nik', '$periode', 'IV', 
                     'BRGKIRIM', 'X', '0', '0', '$kode_pp','".$data[0]['tanggal']."', '".$data[0]['no_dokumen']."', 
-                    '".$data[0]['keterangan']."', 'IDR', '1', '0', '0', '0', '-', '-', '-', '".$data[0]['bukti_kirim']."', '-', '-', 
+                    '".$data[0]['keterangan']."', 'IDR', '1', '0', '0', '0', '-', '-', '-', '-', '-', '-', 
                     '".$data[0]['gudang_asal']."', '".$data[0]['gudang_tujuan']."', '-', null, null, null)";
                 
                 DB::connection($this->sql)->insert($sql2);
@@ -381,7 +386,7 @@ class MutasiController extends Controller {
                     nik1,nik2,nik3,no_ref1,no_ref2,no_ref3,param1,param2,param3,due_date,file_dok,id_sync) 
                     values ('".$data[0]['no_bukti']."', '$kode_lokasi', getdate(), '$nik', '$periode', 'IV', 
                     'BRGTERIMA', 'X', '0', '0', '$kode_pp','".$data[0]['tanggal']."', '".$data[0]['no_dokumen']."', 
-                    '".$data[0]['keterangan']."', 'IDR', '1', '0', '0', '0', '-', '-', '-', '-', '-', '-', 
+                    '".$data[0]['keterangan']."', 'IDR', '1', '0', '0', '0', '-', '-', '-', '".$data[0]['bukti_kirim']."', '-', '-', 
                     '".$data[0]['gudang_asal']."', '".$data[0]['gudang_tujuan']."', '-', null, null, null)";
                 
                 DB::connection($this->sql)->insert($sql2);
@@ -404,6 +409,7 @@ class MutasiController extends Controller {
             DB::connection($this->sql)->commit();
             $success['status'] = true;
             $success['no_bukti'] = $no_bukti;
+            $success['no_kirim'] = $data[0]['bukti_kirim'];
             $success['message'] = "Data Mutasi Barang berhasil disimpan ";
             return response()->json(['success'=>$success], $this->successStatus);
             
