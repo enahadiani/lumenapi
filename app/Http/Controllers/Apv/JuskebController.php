5b998a4989e78cc3d83d61ab8d1f65afc44abccb
enahadiani <enahadiani2@gmail.com>
2021-03-31 13:22:38 +0700

    update apv silo

--- app/Http/Controllers/Apv/JuskebController.php
+++ app/Http/Controllers/Apv/JuskebController.php
@@ -884,30 +884,32 @@ select convert(varchar,e.id) as id,a.no_bukti,case e.status when '2' then 'APPRO
                 $kode_lokasi= $data->kode_lokasi;
             }
 
-            $sql = "select b.no_bukti,b.keterangan,b.tanggal,c.nama,-1 as id,case b.status when 'V' then 'APPROVE' when 'F' then 'RETURN' else '-' end as status,'green' as color
+            $sql = "select b.no_bukti,b.keterangan,b.tanggal,c.nama,-1 as id,case b.status when 'V' then 'APPROVE' when 'F' then 'RETURN' else '-' end as status,'green' as color,d.nik,d.nama as nama_nik
             from apv_juskeb_m a
             inner join apv_ver_m b on a.no_bukti=b.no_juskeb and a.kode_lokasi=b.kode_lokasi
             inner join apv_karyawan d on b.nik_user=d.nik and b.kode_lokasi=d.kode_lokasi
             left join apv_jab c on d.kode_jab=c.kode_jab and d.kode_lokasi=c.kode_lokasi
             where a.kode_lokasi='$kode_lokasi' and a.no_bukti='$no_bukti'
             union all
-            select a.no_bukti,b.keterangan,b.tanggal,c.nama,b.id,case b.status when '2' then 'APPROVE' when '3' then 'RETURN' else '-' end as status,'green' as color
+            select a.no_bukti,b.keterangan,b.tanggal,c.nama,b.id,case b.status when '2' then 'APPROVE' when '3' then 'RETURN' else '-' end as status,'green' as color,d.nik,d.nama as nama_nik
             from apv_flow a
             inner join apv_pesan b on a.no_bukti=b.no_bukti and a.kode_lokasi=b.kode_lokasi and a.no_urut=b.no_urut
             left join apv_jab c on a.kode_jab=c.kode_jab and a.kode_lokasi=c.kode_lokasi
+            inner join apv_karyawan d on a.nik=d.nik and a.kode_lokasi=d.kode_lokasi
             where a.kode_lokasi='$kode_lokasi' and a.no_bukti='$no_bukti'
             union all 
-            select a.no_bukti,a.keterangan,a.tanggal,c.nama,a.id,case a.status when '2' then 'APPROVE' when '3' then 'RETURN' else '-' end as status,'blue' as color
+            select a.no_bukti,a.keterangan,a.tanggal,c.nama,a.id,case a.status when '2' then 'APPROVE' when '3' then 'RETURN' else '-' end as status,'blue' as color,c.nik,c.nama as nama_nik
             from apv_pesan a
             inner join apv_juspo_m d on a.no_bukti=d.no_bukti and a.kode_lokasi=d.kode_lokasi
             left join apv_karyawan c on d.nik_buat=c.nik and d.kode_lokasi=c.kode_lokasi
             where a.kode_lokasi='$kode_lokasi' and d.no_juskeb='$no_bukti' and a.modul='PO'
             union all 
-            select a.no_bukti,b.keterangan,b.tanggal,c.nama,b.id,case b.status when '2' then 'APPROVE' when '3' then 'RETURN' else '-' end as status,'blue' as color
+            select a.no_bukti,b.keterangan,b.tanggal,c.nama,b.id,case b.status when '2' then 'APPROVE' when '3' then 'RETURN' else '-' end as status,'blue' as color,e.nik,e.nama as nama_nik
             from apv_flow a
             inner join apv_pesan b on a.no_bukti=b.no_bukti and a.kode_lokasi=b.kode_lokasi and a.no_urut=b.no_urut
             inner join apv_juspo_m d on a.no_bukti=d.no_bukti and a.kode_lokasi=d.kode_lokasi
             left join apv_jab c on a.kode_jab=c.kode_jab and a.kode_lokasi=c.kode_lokasi
+            inner join apv_karyawan e on a.nik=e.nik and a.kode_lokasi=e.kode_lokasi
             where a.kode_lokasi='$kode_lokasi' and d.no_juskeb='$no_bukti'
             order by id ";
             
