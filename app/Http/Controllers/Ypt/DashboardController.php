31594657d56a5bd1a5befc8fd3fe9a00a6c15468
enahadiani <enahadiani2@gmail.com>
2021-04-12 13:06:07 +0700

    update dash telu

--- app/Http/Controllers/Ypt/DashboardController.php
+++ app/Http/Controllers/Ypt/DashboardController.php
@@ -6598,7 +6598,7 @@ class DashboardController extends Controller
             sum(case when b.jenis_akun='Pendapatan' then -b.n4 else b.n4 end) as real,case sum(n1) when 0 then 0 else (sum(n4)/sum(n1))*100 end as persen  
             from dash_grafik_d a
             left join exs_neraca b on a.kode_neraca=b.kode_neraca and a.kode_lokasi=b.kode_lokasi and a.kode_fs=b.kode_fs
-            $where and a.kode_grafik='GR43' and a.kode_fs='FS4'
+            $where and a.kode_grafik='GR47' and a.kode_fs='FS4'
             group by a.kode_neraca,b.nama
             ");
             $rs = json_decode(json_encode($rs),true);
