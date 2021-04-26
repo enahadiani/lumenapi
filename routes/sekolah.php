18695a95c9d218b89117e2274ecfd03077f8887c
enahadiani <enahadiani2@gmail.com>
2021-04-26 12:07:55 +0700

    update dago

--- routes/sekolah.php
+++ routes/sekolah.php
@@ -313,6 +313,10 @@ $router->group(['middleware' => 'auth:siswa'], function () use ($router) {
 
     $router->get('absen-kelas','Sekolah\AbsenKelasController@show');
     $router->post('absen-kelas','Sekolah\AbsenKelasController@store');
+
+    // UPLOAD RAPORT
+    $router->get('raport-dok-all','Sekolah\UploadRaportController@index');
+    $router->get('raport-dok-siswa','Sekolah\UploadRaportController@loadSiswa');
     
     
 });
