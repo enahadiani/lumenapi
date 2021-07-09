59d8b8f77a98df3c3c1b16c32c4b783b6aab91ac
enahadiani <enahadiani2@gmail.com>
2021-07-09 11:35:57 +0700

    update rumah

--- app/Http/Controllers/Rtrw/RumahController.php
+++ app/Http/Controllers/Rtrw/RumahController.php
@@ -20,6 +20,11 @@ class RumahController extends Controller
     public $guard = 'rtrw';
     public $guard2 = 'satpam';
 
+    public function reverseDate($ymd_or_dmy_date, $org_sep='-', $new_sep='-'){
+        $arr = explode($org_sep, $ymd_or_dmy_date);
+        return $arr[2].$new_sep.$arr[1].$new_sep.$arr[0];
+    }
+
     public function isUnik($isi,$kode_lokasi){
         
         $auth = DB::connection($this->sql)->select("select kode_rumah from rt_rumah where kode_rumah ='".$isi."' and kode_lokasi='".$kode_lokasi."' ");
@@ -64,9 +69,10 @@ class RumahController extends Controller
                 $filter .= "";
             }
             
-            $sql= "select a.kode_rumah,a.keterangan as tipe,a.kode_lokasi,a.rt,a.kode_lokasi as rw,a.blok,a.status_huni,b.nama as nama_pp 
+            $sql= "select a.kode_rumah,a.keterangan as tipe,a.kode_lokasi,a.rt,a.kode_lokasi as rw,a.blok,a.status_huni,b.nama as nama_pp,a.alamat,a.status_huni,a.no_tel,a.emerg_call,a.pbb,a.pln, isnull(c.alamat,'-') as alamat_pemilik, isnull(c.nama_pemilik,'-') as nama_pemilik, isnull(c.no_tel,'-') as no_tel_milik, isnull(convert(varchar,c.tgl_update,103),'-') as tgl_masuk 
             from rt_rumah a 
             left join pp b on a.rt=b.kode_pp and a.kode_lokasi=b.kode_lokasi
+            left join rt_pemilik c on a.kode_rumah=c.kode_rumah and a.kode_lokasi=c.kode_lokasi and c.flag_aktif=1
             where a.kode_lokasi='".$kode_lokasi."' $filter ";
             $res = DB::connection($this->sql)->select($sql);
             $res = json_decode(json_encode($res),true);
@@ -114,7 +120,13 @@ class RumahController extends Controller
             'tipe' => 'required',
             'rw' => 'required',
             'blok' => 'required',
-            'status_huni' => 'required'
+            'status_huni' => 'required',
+            'status_edit' => 'required',
+            'alamat' => 'required',
+            'no_tel' => 'required',
+            'emerg_call' => 'required',
+            'pbb' => 'required',
+            'pln' => 'required',
         ]);
 
         DB::connection($this->sql)->beginTransaction();
@@ -129,7 +141,13 @@ class RumahController extends Controller
             }
             if($this->isUnik($request->kode_rumah,$kode_lokasi)){
 
-                $ins = DB::connection($this->sql)->insert('insert into rt_rumah(kode_rumah,kode_lokasi,rt,rw,blok,status_huni,keterangan) values (?, ?, ?, ?, ?, ?, ?)', array($request->kode_rumah,$kode_lokasi,$request->rt,$request->rw,$request->blok,$request->status_huni,$request->tipe));
+                $ins = DB::connection($this->sql)->insert('insert into rt_rumah(kode_rumah,kode_lokasi,rt,rw,blok,status_huni,keterangan,alamat,no_tel,emerg_call,pln,pbb) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', array($request->kode_rumah,$kode_lokasi,$request->rt,$request->rw,$request->blok,$request->status_huni,$request->tipe,$request->alamat,$request->no_tel,$request->emerg_call,$request->pln,$request->pbb));
+
+                if($request->status_edit == 1){
+                    $upd = DB::connection($this->sql)->update("update rt_pemilik set flag_aktif=0 where kode_rumah='$request->kode_rumah' and kode_lokasi='$request->kode_rw' ");
+
+                    $ins = DB::connection($this->sql)->insert("insert into rt_pemilik(kode_rumah,kode_lokasi,tgl_update,nama_pemilik,alamat,no_tel,tgl_input,nik_user,flag_aktif) values ('$request->kode_rumah','$request->rw','".$this->reverseDate($request->tgl_masuk,"/","-")."','$request->nama_pemilik','$request->alamat_pemilik','$request->no_tel_milik',getdate(),'$nik',1)");
+                }
                 
                 DB::connection($this->sql)->commit();
                 $success['status'] = true;
@@ -187,7 +205,13 @@ class RumahController extends Controller
             'rw' => 'required',
             'blok' => 'required',
             'tipe' => 'required',
-            'status_huni' => 'required'
+            'status_huni' => 'required',
+            'status_edit' => 'required',
+            'alamat' => 'required',
+            'no_tel' => 'required',
+            'emerg_call' => 'required',
+            'pbb' => 'required',
+            'pln' => 'required',
         ]);
 
         DB::connection($this->sql)->beginTransaction();
@@ -206,7 +230,13 @@ class RumahController extends Controller
             ->where('kode_rumah', $request->kode_rumah)
             ->delete();
 
-            $ins = DB::connection($this->sql)->insert('insert into rt_rumah(kode_rumah,kode_lokasi,rt,rw,blok,status_huni,keterangan) values (?, ?, ?, ?, ?, ?, ?)', array($request->kode_rumah,$kode_lokasi,$request->rt,$request->rw,$request->blok,$request->status_huni,$request->tipe));
+            $ins = DB::connection($this->sql)->insert('insert into rt_rumah(kode_rumah,kode_lokasi,rt,rw,blok,status_huni,keterangan,alamat,no_tel,emerg_call,pln,pbb) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', array($request->kode_rumah,$kode_lokasi,$request->rt,$request->rw,$request->blok,$request->status_huni,$request->tipe,$request->alamat,$request->no_tel,$request->emerg_call,$request->pln,$request->pbb));
+
+            if($request->status_edit == 1){
+                $upd = DB::connection($this->sql)->update("update rt_pemilik set flag_aktif=0 where kode_rumah='$request->kode_rumah' and kode_lokasi='$request->rw' ");
+                
+                $ins = DB::connection($this->sql)->insert("insert into rt_pemilik(kode_rumah,kode_lokasi,tgl_update,nama_pemilik,alamat,no_tel,tgl_input,nik_user,flag_aktif) values ('$request->kode_rumah','$request->rw','".$this->reverseDate($request->tgl_masuk,"/","-")."','$request->nama_pemilik','$request->alamat_pemilik','$request->no_tel_milik',getdate(),'$nik',1)");
+            }
                 
             DB::connection($this->sql)->commit();
             $success['status'] = true;
@@ -242,6 +272,11 @@ class RumahController extends Controller
                 $kode_lokasi= $data->kode_lokasi;
             }
             $del = DB::connection($this->sql)->table('rt_rumah')
+            ->where('kode_lokasi', $kode_lokasi)
+            ->where('kode_rumah', $request->kode_rumah)
+            ->delete();
+
+            $del2 = DB::connection($this->sql)->table('rt_pemilik')
             ->where('kode_lokasi', $kode_lokasi)
             ->where('kode_rumah', $request->kode_rumah)
             ->delete();
