ac18380d5451ef1cbd7d1777b251268e9d402c5b
Okta Pascal Ibrahim <oktaiscool@gmail.com>
2021-09-24 09:36:14 +0700

    #INV update api daftar penjualan

---
+++ app/Http/Controllers/Esaku/Inventori/DaftarPenjualanController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Esaku\Inventori;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB; 
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class DaftarPenjualanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public $successStatus = 200;
+    public $sql = 'tokoaws';
+    public $guard = 'toko';
+
+    public function index(Request $request)
+    {
+        try {
+            
+            if($data =  Auth::guard($this->guard)->user()){
+                $nik= $data->nik;
+                $kode_lokasi= $data->kode_lokasi;
+            }
+
+            $sql="SELECT no_jual, nik_user, tanggal, nilai 
+            FROM brg_jualpiu_dloc WHERE kode_lokasi = '".$kode_lokasi."' and nik_user = '".$nik."' and no_close = '-'";
+            $res = DB::connection($this->sql)->select($sql);
+            $res = json_decode(json_encode($res),true);
+            
+            if(count($res) > 0){ //mengecek apakah data kosong atau tidak
+                $success['status'] = true;
+                $success['data'] = $res;
+                $success['message'] = "Success!";     
+            }
+            else{
+                $success['message'] = "Data Kosong!";
+                $success['data'] = [];
+                $success['status'] = false;
+            }
+            return response()->json($success, $this->successStatus);
+        } catch (\Throwable $e) {
+            $success['status'] = false;
+            $success['message'] = "Error ".$e;
+            return response()->json($success, $this->successStatus);
+        }
+        
+    } 
+}
