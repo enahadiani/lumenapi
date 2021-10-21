8b82f1729f9fbb8007a53d9b1729b1f7abe75a0e
Okta Pascal Ibrahim <oktaiscool@gmail.com>
2021-10-21 12:51:00 +0700

    #DASHYPT update api dashboard FP

--- app/Http/Controllers/DashYpt/DashboardFPController.php
+++ app/Http/Controllers/DashYpt/DashboardFPController.php
@@ -89,45 +89,45 @@ class DashboardFPController extENDs Controller
                     $data_pdpt = [
                         "kode_grafik" => $item['kode_grafik'],
                         "nama" => $item['nama'],
-                        "n1" => floatval(number_format((float)$item['n1'], 2, '.', '')),
-                        "n4" => floatval(number_format((float)$item['n4'], 2, '.', '')),
-                        "n5" => floatval(number_format((float)$item['n5'], 2, '.', '')),
-                        "capai" => floatval(number_format((float)$item['capai'], 2, '.', '')),
-                        "ach" => floatval(number_format((float)$item['ach'], 2, '.', '')),
-                        "yoy" => floatval(number_format((float)$item['yoy'], 2, '.', '')),
+                        "n1" => floatval(number_format((float)$item['n1'], 1, '.', '')),
+                        "n4" => floatval(number_format((float)$item['n4'], 1, '.', '')),
+                        "n5" => floatval(number_format((float)$item['n5'], 1, '.', '')),
+                        "capai" => floatval(number_format((float)$item['capai'], 1, '.', '')),
+                        "ach" => floatval(number_format((float)$item['ach'], 1, '.', '')),
+                        "yoy" => floatval(number_format((float)$item['yoy'], 1, '.', '')),
                     ];
                 } elseif($item['kode_grafik'] == 'PI02') {
                     $data_beban = [
                         "kode_grafik" => $item['kode_grafik'],
                         "nama" => $item['nama'],
-                        "n1" => floatval(number_format((float)$item['n1'], 2, '.', '')),
-                        "n4" => floatval(number_format((float)$item['n4'], 2, '.', '')),
-                        "n5" => floatval(number_format((float)$item['n5'], 2, '.', '')),
-                        "capai" => floatval(number_format((float)$item['capai'], 2, '.', '')),
-                        "ach" => floatval(number_format((float)$item['ach'], 2, '.', '')),
-                        "yoy" => floatval(number_format((float)$item['yoy'], 2, '.', '')),
+                        "n1" => floatval(number_format((float)$item['n1'], 1, '.', '')),
+                        "n4" => floatval(number_format((float)$item['n4'], 1, '.', '')),
+                        "n5" => floatval(number_format((float)$item['n5'], 1, '.', '')),
+                        "capai" => floatval(number_format((float)$item['capai'], 1, '.', '')),
+                        "ach" => floatval(number_format((float)$item['ach'], 1, '.', '')),
+                        "yoy" => floatval(number_format((float)$item['yoy'], 1, '.', '')),
                     ];
                 } elseif($item['kode_grafik'] == 'PI03') {
                     $data_shu = [
                         "kode_grafik" => $item['kode_grafik'],
                         "nama" => $item['nama'],
-                        "n1" => floatval(number_format((float)$item['n1'], 2, '.', '')),
-                        "n4" => floatval(number_format((float)$item['n4'], 2, '.', '')),
-                        "n5" => floatval(number_format((float)$item['n5'], 2, '.', '')),
-                        "capai" => floatval(number_format((float)$item['capai'], 2, '.', '')),
-                        "ach" => floatval(number_format((float)$item['ach'], 2, '.', '')),
-                        "yoy" => floatval(number_format((float)$item['yoy'], 2, '.', '')),
+                        "n1" => floatval(number_format((float)$item['n1'], 1, '.', '')),
+                        "n4" => floatval(number_format((float)$item['n4'], 1, '.', '')),
+                        "n5" => floatval(number_format((float)$item['n5'], 1, '.', '')),
+                        "capai" => floatval(number_format((float)$item['capai'], 1, '.', '')),
+                        "ach" => floatval(number_format((float)$item['ach'], 1, '.', '')),
+                        "yoy" => floatval(number_format((float)$item['yoy'], 1, '.', '')),
                     ];
                 } elseif($item['kode_grafik'] == 'PI04') {
                     $data_or = [
                         "kode_grafik" => $item['kode_grafik'],
                         "nama" => $item['nama'],
-                        "n1" => floatval(number_format((float)$item['n1'], 2, '.', '')),
-                        "n4" => floatval(number_format((float)$item['n4'], 2, '.', '')),
-                        "n5" => floatval(number_format((float)$item['n5'], 2, '.', '')),
-                        "capai" => floatval(number_format((float)$item['capai'], 2, '.', '')),
-                        "ach" => floatval(number_format((float)$item['ach'], 2, '.', '')),
-                        "yoy" => floatval(number_format((float)$item['yoy'], 2, '.', '')),
+                        "n1" => floatval(number_format((float)$item['n1'], 1, '.', '')),
+                        "n4" => floatval(number_format((float)$item['n4'], 1, '.', '')),
+                        "n5" => floatval(number_format((float)$item['n5'], 1, '.', '')),
+                        "capai" => floatval(number_format((float)$item['capai'], 1, '.', '')),
+                        "ach" => floatval(number_format((float)$item['ach'], 1, '.', '')),
+                        "yoy" => floatval(number_format((float)$item['yoy'], 1, '.', '')),
                     ];
                 }
             }
