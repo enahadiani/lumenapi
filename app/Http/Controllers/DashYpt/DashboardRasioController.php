2bc14f3766ae31ec9ee75f31436cf5e823afdfbc
enahadiani <enahadiani2@gmail.com>
2021-12-16 13:11:12 +0700

    update dash rasio

--- app/Http/Controllers/DashYpt/DashboardRasioController.php
+++ app/Http/Controllers/DashYpt/DashboardRasioController.php
@@ -80,7 +80,7 @@ class DashboardRasioController extends Controller
 
     public function getRasioYtd(Request $request) {
         $this->validate($request,[
-            'jenis' => 'required',
+            'jenis_rasio' => 'required',
             'periode' => 'required',
             'lokasi' => 'required'
         ]);
@@ -91,23 +91,32 @@ class DashboardRasioController extends Controller
                 $kode_lokasi= $data->kode_lokasi;
             }
             $periode = $request->periode[1];
+            if(isset($r->jenis) && $r->jenis != ""){
+                if($r->jenis == "PRD"){
+                    $n4 = "n6";
+                }else{
+                    $n4 = "n4";
+                }
+            }else{
+                $n4 = "n4";
+            }
             $sql="select a.kode_rasio,a.kode_lokasi,a.kode_neraca,c.rumus, c.nama as nama_rasio,a.nama,b.periode,
             case when a.jenis='C' then isnull(b.nilai2,0)*-1 else isnull(b.nilai2,0) end  as nilai2
             from dash_ypt_rasio_d a
             inner join dash_ypt_rasio_m c on a.kode_rasio=c.kode_rasio and a.kode_lokasi=c.kode_lokasi
-            left join (select a.kode_dash as kode_neraca,a.kode_lokasi,b.periode,sum(b.n4) as nilai2
+            left join (select a.kode_dash as kode_neraca,a.kode_lokasi,b.periode,sum(b.$n4) as nilai2
                     from dash_ypt_neraca_d a
                     inner join exs_neraca b on a.kode_lokasi=b.kode_lokasi and a.kode_neraca=b.kode_neraca and a.kode_fs=b.kode_fs
                     where a.kode_lokasi='$request->lokasi' and a.kode_fs='FS1' and b.periode = '$periode'
                     group by a.kode_dash,a.kode_lokasi,b.periode
                     union all
-                    select a.kode_neraca,a.kode_lokasi,b.periode,sum(b.n4) as nilai2
+                    select a.kode_neraca,a.kode_lokasi,b.periode,sum(b.$n4) as nilai2
                     from dash_ypt_rasio_d a
                     inner join exs_neraca b on a.kode_lokasi=b.kode_lokasi and a.kode_neraca=b.kode_neraca and a.kode_fs=b.kode_fs
-                    where a.kode_lokasi='$request->lokasi' and a.kode_fs='FS1' and a.kode_rasio='$request->jenis' and b.periode = '$periode'
+                    where a.kode_lokasi='$request->lokasi' and a.kode_fs='FS1' and a.kode_rasio='$request->jenis_rasio' and b.periode = '$periode'
                     group by a.kode_neraca,a.kode_lokasi,b.periode
                     )b on a.kode_neraca=b.kode_neraca and a.kode_lokasi=b.kode_lokasi
-            where a.kode_lokasi='$request->lokasi' and a.kode_fs='FS1'  and a.kode_rasio='$request->jenis'";
+            where a.kode_lokasi='$request->lokasi' and a.kode_fs='FS1'  and a.kode_rasio='$request->jenis_rasio'";
             $res = DB::connection($this->db)->select($sql);
             $res = json_decode(json_encode($res),true);
             
@@ -176,7 +185,7 @@ class DashboardRasioController extends Controller
 
     public function getRasioYoY(Request $request) {
         $this->validate($request,[
-            'jenis' => 'required',
+            'jenis_rasio' => 'required',
             'periode' => 'required',
             'lokasi' => 'required'
         ]);
@@ -190,23 +199,32 @@ class DashboardRasioController extends Controller
             $tahun = intval(substr($request->periode[1],0,4))-1;
             $bulan = substr($request->periode[1],4,2);
             $perAwal = $tahun.$bulan;
+            if(isset($r->jenis) && $r->jenis != ""){
+                if($r->jenis == "PRD"){
+                    $n4 = "n6";
+                }else{
+                    $n4 = "n4";
+                }
+            }else{
+                $n4 = "n4";
+            }
             $sql="select a.kode_rasio,a.kode_lokasi,a.kode_neraca,c.rumus, c.nama as nama_rasio,a.nama,b.periode,
             case when a.jenis='C' then isnull(b.nilai2,0)*-1 else isnull(b.nilai2,0) end  as nilai2
             from dash_ypt_rasio_d a
             inner join dash_ypt_rasio_m c on a.kode_rasio=c.kode_rasio and a.kode_lokasi=c.kode_lokasi
-            left join (select a.kode_dash as kode_neraca,a.kode_lokasi,b.periode,sum(b.n4) as nilai2
+            left join (select a.kode_dash as kode_neraca,a.kode_lokasi,b.periode,sum(b.$n4) as nilai2
                     from dash_ypt_neraca_d a
                     inner join exs_neraca b on a.kode_lokasi=b.kode_lokasi and a.kode_neraca=b.kode_neraca and a.kode_fs=b.kode_fs
                     where a.kode_lokasi='$request->lokasi' and a.kode_fs='FS1' and b.periode in ('$perAwal','$periode')
                     group by a.kode_dash,a.kode_lokasi,b.periode
                     union all
-                    select a.kode_neraca,a.kode_lokasi,b.periode,sum(b.n4) as nilai2
+                    select a.kode_neraca,a.kode_lokasi,b.periode,sum(b.$n4) as nilai2
                     from dash_ypt_rasio_d a
                     inner join exs_neraca b on a.kode_lokasi=b.kode_lokasi and a.kode_neraca=b.kode_neraca and a.kode_fs=b.kode_fs
-                    where a.kode_lokasi='$request->lokasi' and a.kode_fs='FS1' and a.kode_rasio='$request->jenis' and b.periode in ('$perAwal','$periode')
+                    where a.kode_lokasi='$request->lokasi' and a.kode_fs='FS1' and a.kode_rasio='$request->jenis_rasio' and b.periode in ('$perAwal','$periode')
                     group by a.kode_neraca,a.kode_lokasi,b.periode
                     )b on a.kode_neraca=b.kode_neraca and a.kode_lokasi=b.kode_lokasi
-            where a.kode_lokasi='$request->lokasi' and a.kode_fs='FS1'  and a.kode_rasio='$request->jenis'";
+            where a.kode_lokasi='$request->lokasi' and a.kode_fs='FS1'  and a.kode_rasio='$request->jenis_rasio'";
             $res = DB::connection($this->db)->select($sql);
             $res = json_decode(json_encode($res),true);
             
@@ -278,7 +296,7 @@ class DashboardRasioController extends Controller
 
     public function getRasioTahun(Request $request) {
         $this->validate($request,[
-            'jenis' => 'required',
+            'jenis_rasio' => 'required',
             'periode' => 'required',
             'lokasi' => 'required'
         ]);
@@ -305,24 +323,33 @@ class DashboardRasioController extends Controller
                 $tahun++;
             }
             $success['ctg'] = $ctg;
+            if(isset($r->jenis) && $r->jenis != ""){
+                if($r->jenis == "PRD"){
+                    $n4 = "n6";
+                }else{
+                    $n4 = "n4";
+                }
+            }else{
+                $n4 = "n4";
+            }
 
             $sql="select a.kode_rasio,a.kode_lokasi,a.kode_neraca,c.rumus, c.nama as nama_rasio,a.nama,b.periode,
             case when a.jenis='C' then isnull(b.nilai2,0)*-1 else isnull(b.nilai2,0) end  as nilai2
             from dash_ypt_rasio_d a
             inner join dash_ypt_rasio_m c on a.kode_rasio=c.kode_rasio and a.kode_lokasi=c.kode_lokasi
-            left join (select a.kode_dash as kode_neraca,a.kode_lokasi,b.periode,sum(b.n4) as nilai2
+            left join (select a.kode_dash as kode_neraca,a.kode_lokasi,b.periode,sum(b.$n4) as nilai2
                     from dash_ypt_neraca_d a
                     inner join exs_neraca b on a.kode_lokasi=b.kode_lokasi and a.kode_neraca=b.kode_neraca and a.kode_fs=b.kode_fs
                     where a.kode_lokasi='$request->lokasi' and a.kode_fs='FS1' and b.periode in ($thn)
                     group by a.kode_dash,a.kode_lokasi,b.periode
                     union all
-                    select a.kode_neraca,a.kode_lokasi,b.periode,sum(b.n4) as nilai2
+                    select a.kode_neraca,a.kode_lokasi,b.periode,sum(b.$n4) as nilai2
                     from dash_ypt_rasio_d a
                     inner join exs_neraca b on a.kode_lokasi=b.kode_lokasi and a.kode_neraca=b.kode_neraca and a.kode_fs=b.kode_fs
-                    where a.kode_lokasi='$request->lokasi' and a.kode_fs='FS1' and a.kode_rasio='$request->jenis' and b.periode in ($thn)
+                    where a.kode_lokasi='$request->lokasi' and a.kode_fs='FS1' and a.kode_rasio='$request->jenis_rasio' and b.periode in ($thn)
                     group by a.kode_neraca,a.kode_lokasi,b.periode
                     )b on a.kode_neraca=b.kode_neraca and a.kode_lokasi=b.kode_lokasi
-            where a.kode_lokasi='$request->lokasi' and a.kode_fs='FS1'  and a.kode_rasio='$request->jenis'";
+            where a.kode_lokasi='$request->lokasi' and a.kode_fs='FS1'  and a.kode_rasio='$request->jenis_rasio'";
             $res = DB::connection($this->db)->select($sql);
             $res = json_decode(json_encode($res),true);
             
