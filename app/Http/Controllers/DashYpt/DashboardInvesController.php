eabd4ece1168b97cf86728433157dbf562d67316
enahadiani <enahadiani2@gmail.com>
2021-12-20 10:19:16 +0700

    update dash inves

--- app/Http/Controllers/DashYpt/DashboardInvesController.php
+++ app/Http/Controllers/DashYpt/DashboardInvesController.php
@@ -68,51 +68,29 @@ class DashboardInvesController extends Controller {
             }else{
                 $filter_neraca = "";
             }
-            $sql = "select a.kode_lokasi,
-            sum(a.n2) as n1,
-            sum(a.n4) as n2,
-            case when sum(a.n2) <> 0 then (sum(a.n4)/sum(a.n2))*100 else 0 end as capai
+            $sql = "select a.kode_lokasi,sum(a.n1) as rka_tahun,sum(a.n2) as rka_bulan,sum(a.n6) as real_bulan,sum(a.n7) as real_rev,
+            case when sum(a.n2) <> 0 then (sum(a.n4)/sum(a.n3))*100 else 0 end as capai_ytd,
+            case when sum(a.n1) <> 0 then (sum(a.n4)/sum(a.n1))*100 else 0 end as capai_tahun,
+            case when sum(a.n5) <> 0 then ((sum(a.n4)-sum(a.n5))/sum(a.n5))*100 else 0 end as yoy
             from exs_neraca a
             inner join dash_ypt_neraca_d b on a.kode_neraca=b.kode_neraca and a.kode_fs=b.kode_fs
-            $where and b.kode_dash='DP02' and (a.n2<>0 or a.n4<>0) $filter_neraca
+            $where and b.kode_dash='DP02'
             group by a.kode_lokasi
             ";
-            $ytd = DB::connection($this->db)->select($sql);
-
-            $sql = "select a.kode_lokasi,
-            sum(a.n1) as n1,
-            sum(a.n4) as n2,
-            case when sum(a.n1) <> 0 then (sum(a.n4)/sum(a.n1))*100 else 0 end as capai
-            from exs_neraca a
-            inner join dash_ypt_neraca_d b on a.kode_neraca=b.kode_neraca and a.kode_fs=b.kode_fs
-            $where and b.kode_dash='DP02' and (a.n1<>0 or a.n4<>0) $filter_neraca
-                        group by a.kode_lokasi
-            ";
-            $tahun = DB::connection($this->db)->select($sql);
-
-            $sql = "select a.kode_lokasi,
-            sum(a.n4) as n1,
-            sum(a.n5) as n2,
-            case when sum(a.n5) <> 0 then (sum(a.n4)/sum(a.n5))*100 else 0 end as capai
-            from exs_neraca a
-            inner join dash_ypt_neraca_d b on a.kode_neraca=b.kode_neraca and a.kode_fs=b.kode_fs
-            $where and b.kode_dash='DP02' and (a.n4<>0 or a.n5<>0) $filter_neraca
-                        group by a.kode_lokasi
-            ";
-            $ach = DB::connection($this->db)->select($sql);
+            $res = DB::connection($this->db)->select($sql);
 
             $success['status'] = true;
             $success['message'] = "Success!";
             $success['data'][0] = array(
-                'persen_ytd' => (count($ytd) > 0 ? $ytd[0]->capai : 0),
-                'rka_ytd' => (count($ytd) > 0 ? $ytd[0]->n1 : 0),
-                'real_ytd' => (count($ytd) > 0 ? $ytd[0]->n2 : 0),
-                'persen_tahun' => (count($tahun) > 0 ? $tahun[0]->capai : 0),
-                'rka_tahun' => (count($tahun) > 0 ? $tahun[0]->n1 : 0),
-                'real_tahun' => (count($tahun) > 0 ? $tahun[0]->n2 : 0),
-                'persen_ach' => (count($ach) > 0 ? $ach[0]->capai : 0),
-                'ach_now' => (count($ach) > 0 ? $ach[0]->n1 : 0),
-                'ach_lalu' => (count($ach) > 0 ? $ach[0]->n2 : 0)
+                'persen_ytd' => (count($res) > 0 ? $res[0]->capai_ytd : 0),
+                'rka_ytd' => (count($res) > 0 ? $res[0]->rka_bulan : 0),
+                'real_ytd' => (count($res) > 0 ? $res[0]->real_bulan : 0),
+                'persen_tahun' => (count($res) > 0 ? $res[0]->capai_tahun : 0),
+                'rka_tahun' => (count($res) > 0 ? $res[0]->rka_tahun : 0),
+                'real_tahun' => (count($res) > 0 ? $res[0]->real_bulan : 0),
+                'persen_ach' => (count($res) > 0 ? $res[0]->yoy : 0),
+                'real_now' => (count($res) > 0 ? $res[0]->real_bulan : 0),
+                'real_lalu' => (count($res) > 0 ? $res[0]->real_rev : 0)
             );
 
             return response()->json($success, $this->successStatus); 
@@ -293,12 +271,6 @@ class DashboardInvesController extends Controller {
             $thn = "";
             for($x=0;$x < 5;$x++){
                 array_push($ctg,$tahun);
-                if($x == 0){
-                    $thn .= "'".$tahun.$bulan."'";
-                }else{
-                    
-                    $thn .= ","."'".$tahun.$bulan."'";
-                }
                 $tahun++;
             }
            
@@ -310,11 +282,11 @@ class DashboardInvesController extends Controller {
             */
             $sql="select a.kode_lokasi, a.nama, a.skode, isnull(b.n1,0) as n1, isnull(b.n2,0) as n2, isnull(b.n3,0) as n3, isnull(b.n4,0) as n4, isnull(b.n5,0) as n5
             from dash_ypt_lokasi a
-            left join (select a.kode_lokasi,sum(case when substring(a.periode,1,4)='2017' then a.n4 else 0 end) as n1,
-                           sum(case when substring(a.periode,1,4)='2018' then a.n4 else 0 end) as n2,
-                           sum(case when substring(a.periode,1,4)='2019' then a.n4 else 0 end) as n3,
-                           sum(case when substring(a.periode,1,4)='2020' then a.n4 else 0 end) as n4,
-                           sum(case when substring(a.periode,1,4)='2021' then a.n4 else 0 end) as n5
+            left join (select a.kode_lokasi,sum(case when substring(a.periode,1,4)='".$ctg[0]."' then a.n4 else 0 end) as n1,
+                           sum(case when substring(a.periode,1,4)='".$ctg[1]."' then a.n4 else 0 end) as n2,
+                           sum(case when substring(a.periode,1,4)='".$ctg[2]."' then a.n4 else 0 end) as n3,
+                           sum(case when substring(a.periode,1,4)='".$ctg[3]."' then a.n4 else 0 end) as n4,
+                           sum(case when substring(a.periode,1,4)='".$ctg[4]."' then a.n4 else 0 end) as n5
                     from exs_neraca a
                     inner join dash_ypt_neraca_d b on a.kode_neraca=b.kode_neraca and a.kode_fs=b.kode_fs
                     where b.kode_dash='DP02' 
@@ -330,11 +302,11 @@ class DashboardInvesController extends Controller {
                     $series[$i] = array('name' => $dt['nama'], 'data' => array());
                 }
                 $data = array(
-                floatval($dt['n1']) - $pengurang, 
-                floatval($dt['n2']) - $pengurang, 
-                floatval($dt['n3']) - $pengurang, 
-                floatval($dt['n4']) - $pengurang, 
-                floatval($dt['n5']) - $pengurang
+                    floatval($dt['n1']), 
+                    floatval($dt['n2']), 
+                    floatval($dt['n3']), 
+                    floatval($dt['n4']), 
+                    floatval($dt['n5'])
                 );
                 $series[$i]['data'] = $data;
                 //$pengurang+= 567891011;
@@ -344,7 +316,8 @@ class DashboardInvesController extends Controller {
             $success['message'] = "Success!";
             $success['data'] = array(
                 'kategori' => $ctg,
-                'series' => $series
+                'series' => $series,
+                'res' => $res
             );
 
             return response()->json($success, $this->successStatus); 
