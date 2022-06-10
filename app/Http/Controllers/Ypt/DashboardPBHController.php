dd6158d5405010c9b8634ad24dda06102decd3c7
enahadiani <devsaku5@gmail.com>
2022-06-10 17:58:28 +0700

    update dash telu

--- app/Http/Controllers/Ypt/DashboardPBHController.php
+++ app/Http/Controllers/Ypt/DashboardPBHController.php
@@ -61,31 +61,41 @@ class DashboardPBHController extends Controller
                 $filter_pp = " ";
             }
 
+            if(isset($r->kode_bidang) && $r->kode_bidang != ""){
+                $filter_bidang = " and p.kode_bidang='$r->kode_bidang' ";
+            }else{
+                $filter_bidang = " ";
+            }
+
             $sql = "select a.kode_lokasi,sum(a.nilai) as nilai,count(a.no_aju) as jml
             from it_aju_m a
             inner join ver_m d on a.no_ver=d.no_ver and a.kode_lokasi=d.kode_lokasi
-            where a.kode_lokasi='$kode_lokasi' and substring(a.periode,1,4)='$tahun' $filter_pp
+            inner join pp p on a.kode_pp=p.kode_pp and a.kode_lokasi=p.kode_lokasi
+            where a.kode_lokasi='$kode_lokasi' and substring(a.periode,1,4)='$tahun' $filter_pp $filter_bidang
             group by a.kode_lokasi";
             $select = DB::connection($this->db)->select($sql);
 
             $sql2 = "select a.kode_lokasi,sum(a.nilai) as nilai,count(a.no_aju) as jml
             from it_aju_m a
             inner join fiat_m b on a.no_fiat=b.no_fiat and a.kode_lokasi=b.kode_lokasi
-            where a.kode_lokasi='$kode_lokasi' and substring(a.periode,1,4)='$tahun' $filter_pp
+            inner join pp p on a.kode_pp=p.kode_pp and a.kode_lokasi=p.kode_lokasi
+            where a.kode_lokasi='$kode_lokasi' and substring(a.periode,1,4)='$tahun' $filter_pp $filter_bidang
             group by a.kode_lokasi";
             $select2 = DB::connection($this->db)->select($sql2);
 
             $sql3 = "select a.kode_lokasi,sum(a.nilai) as nilai,count(a.no_aju) as jml
             from it_aju_m a
             inner join it_spb_m b on a.no_spb=b.no_spb and a.kode_lokasi=b.kode_lokasi
-            where a.kode_lokasi='$kode_lokasi' and substring(a.periode,1,4)='$tahun' $filter_pp
+            inner join pp p on a.kode_pp=p.kode_pp and a.kode_lokasi=p.kode_lokasi
+            where a.kode_lokasi='$kode_lokasi' and substring(a.periode,1,4)='$tahun' $filter_pp $filter_bidang
             group by a.kode_lokasi";
             $select3 = DB::connection($this->db)->select($sql3);
 
             $sql4 = "select a.kode_lokasi,sum(a.nilai) as nilai,count(a.no_aju) as jml
             from it_aju_m a
             inner join kas_m b on a.no_kas=b.no_kas and a.kode_lokasi=b.kode_lokasi
-            where a.kode_lokasi='$kode_lokasi' and substring(a.periode,1,4)='$tahun' $filter_pp
+            inner join pp p on a.kode_pp=p.kode_pp and a.kode_lokasi=p.kode_lokasi
+            where a.kode_lokasi='$kode_lokasi' and substring(a.periode,1,4)='$tahun' $filter_pp $filter_bidang
             group by a.kode_lokasi";
             $select4 = DB::connection($this->db)->select($sql4);
 
@@ -134,13 +144,20 @@ class DashboardPBHController extends Controller
                 $filter_pp = " ";
             }
 
+            if(isset($r->kode_bidang) && $r->kode_bidang != ""){
+                $filter_bidang = " and p.kode_bidang='$r->kode_bidang' ";
+            }else{
+                $filter_bidang = " ";
+            }
+
             $sql = "select a.kode_lokasi,sum(case when a.jenis='OFFLINE' then a.jml else 0 end) as jml_offline,
             sum(case when a.jenis='ONLINE' then a.jml else 0 end) as jml_online
             from (
             select a.kode_lokasi,b.jenis,count(a.no_aju) as jml
             from it_aju_m a
             inner join it_ajuapp_m b on a.no_aju=b.no_aju and a.kode_lokasi=b.kode_lokasi
-            where a.kode_lokasi='$kode_lokasi' and substring(a.periode,1,4)='$tahun' $filter_pp
+            inner join pp p on a.kode_pp=p.kode_pp and a.kode_lokasi=p.kode_lokasi
+            where a.kode_lokasi='$kode_lokasi' and substring(a.periode,1,4)='$tahun' $filter_pp $filter_bidang
             group by a.kode_lokasi,b.jenis
                 )a
             group by a.kode_lokasi";
@@ -193,10 +210,18 @@ class DashboardPBHController extends Controller
             }else{
                 $filter_pp = " ";
             }
+
+            if(isset($r->kode_bidang) && $r->kode_bidang != ""){
+                $filter_bidang = " and p.kode_bidang='$r->kode_bidang' ";
+            }else{
+                $filter_bidang = " ";
+            }
+
             $sql="select a.kode_lokasi,b.periode,substring(dbo.fnNamaBulan(b.periode),1,3) as nama,sum(a.nilai) as nilai,count(a.no_aju) as jml
             from it_aju_m a
             inner join kas_m b on a.no_kas=b.no_kas and a.kode_lokasi=b.kode_lokasi
-            where a.kode_lokasi='$kode_lokasi' and substring(b.periode,1,4)='$tahun_seb' $filter_pp
+            inner join pp p on a.kode_pp=p.kode_pp and a.kode_lokasi=p.kode_lokasi
+            where a.kode_lokasi='$kode_lokasi' and substring(b.periode,1,4)='$tahun_seb' $filter_pp $filter_bidang
             group by a.kode_lokasi,b.periode
             order by a.kode_lokasi,b.periode
             ";
@@ -205,7 +230,8 @@ class DashboardPBHController extends Controller
             $sql2="select a.kode_lokasi,b.periode,substring(dbo.fnNamaBulan(b.periode),1,3) as nama,sum(a.nilai) as nilai,count(a.no_aju) as jml
             from it_aju_m a
             inner join kas_m b on a.no_kas=b.no_kas and a.kode_lokasi=b.kode_lokasi
-            where a.kode_lokasi='$kode_lokasi' and substring(b.periode,1,4)='$tahun' $filter_pp
+            inner join pp p on a.kode_pp=p.kode_pp and a.kode_lokasi=p.kode_lokasi
+            where a.kode_lokasi='$kode_lokasi' and substring(b.periode,1,4)='$tahun' $filter_pp $filter_bidang
             group by a.kode_lokasi,b.periode
             order by a.kode_lokasi,b.periode
             ";
@@ -247,6 +273,78 @@ class DashboardPBHController extends Controller
             $success['message'] = "Error ".$e;
             return response()->json($success, $this->successStatus);
         }
+    }
+
+    public function getBidang(Request $request)
+    {
+        try {
+            
+            if($data =  Auth::guard($this->guard)->user()){
+                $nik_user= $data->nik;
+                $kode_lokasi= $data->kode_lokasi;
+            }
+
+            $res = DB::connection($this->db)->select("select kode_bidang,nama from bidang where kode_lokasi='".$kode_lokasi."'
+            ");
+            $res = json_decode(json_encode($res),true);
+            
+            if(count($res) > 0){ //mengecek apakah data kosong atau tidak
+                $success['status'] = true;
+                $success['data'] = $res;
+                $success['message'] = "Success!";
+                return response()->json($success, $this->successStatus);     
+            }
+            else{
+                $success['message'] = "Data Kosong!";
+                $success['data'] = [];
+                $success['status'] = true;
+                return response()->json($success, $this->successStatus);
+            }
+        } catch (\Throwable $e) {
+            $success['status'] = false;
+            $success['data'] = "Error ".$e;
+            $success['message'] = "Error ".$e;
+            return response()->json($success, $this->successStatus);
+        }
+        
+    }
+
+    public function getPP(Request $request)
+    {
+        try {
+            
+            if($data =  Auth::guard($this->guard)->user()){
+                $nik_user= $data->nik;
+                $kode_lokasi= $data->kode_lokasi;
+            }
+            if(isset($request->kode_bidang) && $request->kode_bidang != ""){
+                $filter = " and kode_bidang = '$request->kode_bidang' ";
+            }else{
+                $filter = "";
+            }
+
+            $res = DB::connection($this->db)->select("select kode_pp,nama from pp where kode_lokasi='".$kode_lokasi."' $filter
+            ");
+            $res = json_decode(json_encode($res),true);
+            
+            if(count($res) > 0){ //mengecek apakah data kosong atau tidak
+                $success['status'] = true;
+                $success['data'] = $res;
+                $success['message'] = "Success!";
+                return response()->json($success, $this->successStatus);     
+            }
+            else{
+                $success['message'] = "Data Kosong!";
+                $success['data'] = [];
+                $success['status'] = true;
+                return response()->json($success, $this->successStatus);
+            }
+        } catch (\Throwable $e) {
+            $success['status'] = false;
+            $success['message'] = "Error ".$e;
+            return response()->json($success, $this->successStatus);
+        }
+        
     }
 
 }
